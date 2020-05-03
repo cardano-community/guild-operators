07d8f9e29e619e89d69524d5cf80f1b096ab6861
Markus <gufmar@gmail.com>
2020-05-03 22:33:42 +0200

    load CNODE_PATH variable from bashrc (ignore shellcheck)

--- files/ptn0/scripts/prereqs.sh
+++ files/ptn0/scripts/prereqs.sh
@@ -69,7 +69,8 @@ if grep -q CNODE_HOME ~/.bashrc; then
 else
   echo "Setting up Environment Variable"
   echo "export CNODE_HOME=${CNODE_HOME}" >> ~/.bashrc
-  . ~/.bashrc
+  # shellcheck source=/dev/null
+  . "${HOME}/.bashrc"
 fi
 $sudo mkdir -p $CNODE_HOME/files $CNODE_HOME/db $CNODE_HOME/logs $CNODE_HOME/scripts $CNODE_HOME/sockets $CNODE_HOME/priv
 $sudo chown -R "$USER":"$USER" "$CNODE_HOME"
