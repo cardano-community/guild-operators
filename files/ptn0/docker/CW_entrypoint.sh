a587a2c0a0ce66fb3fe7587df4ce7d19115dcc55
Michael <michael.dogali@gmail.com>
2020-05-09 00:03:09 +0200

    Update CW_entrypoint.sh
    $CWALLET_PORT updated

--- files/ptn0/docker/CW_entrypoint.sh
+++ files/ptn0/docker/CW_entrypoint.sh
@@ -1,29 +1,29 @@
 
 export CNODE_HOME=/opt/cardano/cnode 
 export CNODE_PORT=9000 
- 
+export CWALLET_PORT=8090 
+
 if [[ -d /configuration ]]; then
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
 	--database /wallet-db \
 	--listen-address 0.0.0.0 \
-    	--port 8090 \
+    	--port $CWALLET_PORT \
 	--mainnet $@
 elif [[ "$NETWORK" == "testnet" ]]; then
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
 	--database /wallet-db \
 	--listen-address 0.0.0.0 \
-    	--port 8090 \
+    	--port $CWALLET_PORT \
 	--testnet $@
 elif [[ "$NETWORK" == "mainnet" ]]; then
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
 	--database /wallet-db \
 	--listen-address 0.0.0.0 \
-    	--port 8090 \
+    	--port $CWALLET_PORT \
 	--mainnet $@
 else
   echo "Please set a NETWORK environment variable to one of: mainnet/testnet"
-  echo "Or mount a /configuration volume containing: configuration.yaml, genesis.json, and topology.json + Pool.cert, Pool.key for active nodes"
 fi
