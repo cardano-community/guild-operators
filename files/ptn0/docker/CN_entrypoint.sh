e7262a4f325914b1ed8e3f2a149e34dfef3c2000
Michael <michael.dogali@gmail.com>
2020-05-09 15:19:17 +0200

    Update CN_entrypoint.sh

--- files/ptn0/docker/CN_entrypoint.sh
+++ files/ptn0/docker/CN_entrypoint.sh
@@ -11,27 +11,27 @@ if [[ -d /configuration ]]; then
   exec cardano-node run \
     --config /configuration/configuration.yaml \
     --database-path /tmp/db \
-    --host-addr 127.0.0.1 \
+    --host-addr `curl ifconfig.me` \
+    --signing-key /configuration/Pool.key \
+    --delegation-certificate /configuration/Pool.cert \
     --port $CNODE_PORT \
     --socket-path /ipc/node.socket \
     --topology /configuration/topology.json $@
-elif [[ "$NETWORK" == "passive" ]]; then
+elif [[ "$NETWORK" == "testnet" ]]; then
   exec cardano-node run \
   --config $CNODE_HOME/files/ptn0.yaml \
-  --database-path $CNODE_HOME/db \
+  --database-path $CNODE_HOME/testnet-db \
   --host-addr `curl ifconfig.me` \
   --port $CNODE_PORT \
-  --topology $CNODE_HOME/files/topology.json $@
-elif [[ "$NETWORK" == "active" ]]; then
+  --topology $CNODE_HOME/files/testnet-topology.json $@
+elif [[ "$NETWORK" == "mainnet" ]]; then
   exec cardano-node run \
     --config /configuration/configuration.yaml \
-    --database-path /tmp/db \
-    --signing-key /configuration/Pool.key \
-    --delegation-certificate /configuration/Pool.cert \
+    --database-path /tmp/mainnet-db 
     --host-addr `curl ifconfig.me` \
     --port $CNODE_PORT \
     --socket-path /ipc/node.socket \
-    --topology /configuration/topology.json $@
+    --topology /configuration/mainnet-topology.json $@
 else
   echo "Please set a NETWORK environment variable to one of: active/passive"
   echo "Or mount a /configuration volume containing: configuration.yaml, genesis.json, and topology.json + Pool.cert, Pool.key for active nodes"
