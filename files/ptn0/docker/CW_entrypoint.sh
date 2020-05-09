fcfc47db699a3fad02be6844274d0f0940fa1d7a
Michael <michael.dogali@gmail.com>
2020-05-09 15:32:04 +0200

    Update CW_entrypoint.sh

--- files/ptn0/docker/CW_entrypoint.sh
+++ files/ptn0/docker/CW_entrypoint.sh
@@ -8,24 +8,24 @@ export CNODE_HOME=/opt/cardano/cnode
 export CNODE_PORT=9000 
 export CWALLET_PORT=8090 
 
-if [[ -d /configuration ]]; then
+if [[ -d /config ]]; then
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
-	--database /wallet-db \
+	--database /config/wallet-db \
 	--listen-address 0.0.0.0 \
     	--port $CWALLET_PORT \
 	--mainnet $@
 elif [[ "$NETWORK" == "testnet" ]]; then
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
-	--database /wallet-db \
+	--database /wallet-testnet-db \
 	--listen-address 0.0.0.0 \
     	--port $CWALLET_PORT \
 	--testnet $@
 elif [[ "$NETWORK" == "mainnet" ]]; then
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
-	--database /wallet-db \
+	--database /wallet-mainnet-db \
 	--listen-address 0.0.0.0 \
     	--port $CWALLET_PORT \
 	--mainnet $@
