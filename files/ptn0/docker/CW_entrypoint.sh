76f632d004b0aa70944ca7d0715bb43adbf07f86
Michael <michael.dogali@gmail.com>
2020-05-10 19:40:38 +0200

    Update CW_entrypoint.sh

--- files/ptn0/docker/CW_entrypoint.sh
+++ files/ptn0/docker/CW_entrypoint.sh
@@ -15,19 +15,11 @@ if [[ -d /config ]]; then
 	--listen-address 0.0.0.0 \
     	--port $CWALLET_PORT \
 	--mainnet $@
-elif [[ "$NETWORK" == "testnet" ]]; then
-  exec ln -s /opt/cardano/cnode/files/testnet-topology.json /config/topology.json \
-  exec cardano-wallet-byron serve \
-	--node-socket /ipc/node.socket \
-	--database /wallet-testnet-db \
-	--listen-address 0.0.0.0 \
-    	--port $CWALLET_PORT \
-	--testnet $@
 elif [[ "$NETWORK" == "mainnet" ]]; then
   exec ln -s /opt/cardano/cnode/files/mainnet-topology.yaml /config/topology.yaml \
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
-	--database /wallet-mainnet-db \
+	--database /tmp/wallet-mainnet-db \
 	--listen-address 0.0.0.0 \
     	--port $CWALLET_PORT \
 	--mainnet $@
@@ -35,10 +27,10 @@ elif [[ "$NETWORK" == "ptn0" ]]; then
   exec ln -s /opt/cardano/cnode/files/topology.json /config/topology.json \
   exec cardano-wallet-byron serve \
 	--node-socket /ipc/node.socket \
-	--database /wallet-ptn0-db \
+	--database /tmp/wallet-ptn0-db \
 	--listen-address 0.0.0.0 \
     	--port $CWALLET_PORT \
 	--mainnet $@
 else
-  echo "Please set a NETWORK environment variable to one of: mainnet/testnet"
+  echo "Please set a NETWORK environment variable to one of: mainnet/ptn0"
 fi
