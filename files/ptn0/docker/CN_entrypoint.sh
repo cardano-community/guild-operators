b5bc8df83606c35fc1498419359446cd37c7d95b
Michael <michael.dogali@gmail.com>
2020-05-10 14:19:48 +0200

    Update CN_entrypoint.sh

--- files/ptn0/docker/CN_entrypoint.sh
+++ files/ptn0/docker/CN_entrypoint.sh
@@ -9,15 +9,15 @@ export CNODE_PORT=9000
 
 if [[ -d /configuration ]]; then
   exec cardano-node run \
-	--topology /configuration/topology.json \
-	--shelley-kes-key /configuration/Pool.skey \
- 	--shelley-operational-certificate /configuration/Pool.cert \
-	--shelley-vrf-key /configuration/vrf.skey \
+  --topology /configuration/topology.json \
+  --shelley-kes-key /configuration/Pool.skey \
+  --shelley-operational-certificate /configuration/Pool.cert \
+  --shelley-vrf-key /configuration/vrf.skey \
   --config /configuration/configuration.yaml \
-	--database-path $CNODE_HOME/files/db \
+  --database-path $CNODE_HOME/files/db \
   --port $CNODE_PORT \
-	--socket-path /ipc/node.socket \
-	--host-addr `curl ifconfig.me` $@ 
+  --socket-path /ipc/node.socket \
+  --host-addr `curl ifconfig.me` $@ 
 elif [[ "$NETWORK" == "testnet" ]]; then
   exec cardano-node run \
   --config $CNODE_HOME/files/testnet.yaml \
@@ -42,7 +42,7 @@ elif [[ "$NETWORK" == "ptn0" ]]; then
     --port $CNODE_PORT \
     --socket-path /ipc/node.socket \
     --topology $CNODE_HOME/files/topology.json \
-	  --shelley-vrf-key /configuration/vrf.skey $@
+    --shelley-vrf-key /configuration/vrf.skey $@
 else
   echo "Please set a NETWORK environment variable to one of: testnet/mainnet/byron/ptn0"
   echo "Or mount a /configuration volume containing: configuration.yaml, genesis.json, and topology.json + Pool.cert, Pool.key for active nodes"
