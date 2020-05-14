6b29d7974ad3916a0fc0d87c4ac0a7e30e3bd8f1
Greg Beresnev <greg.beresnev@yarris.com>
2020-05-14 13:56:17 +1000

    Improvements to the script

--- scripts/cnode-helper-scripts/sendADA.sh
+++ scripts/cnode-helper-scripts/sendADA.sh
@@ -2,22 +2,32 @@
 
 . $(dirname $0)/env
 
-function cleanupAndExit() {
-	echo "Exiting after cleanup"
+function cleanup() {
 	rm -rf /tmp/balance.txt
 	rm -rf /tmp/protparams.json
 	rm -rf /tmp/tx.signed
+}
+
+function myexit() {
+	if [ ! -z "$1" ]; then
+		echo "Exiting: $1"
+	fi
 	exit 1
 }
+
+# start with a clean slate
+cleanup
+
+NWMAGIC=`cat ${GENESIS_JSON} | jq .networkMagic`
 
 case $# in
   4 ) tx="$1";
     outaddr="$2";
     lovelace="$(( $3 * 1000000 ))";
     from_key="$4";
-    from_addr="");;
+    from_addr="";
   * ) cat >&2 <<EOF
-Usage:  $(basename $0) <Tx-File to Create for submission> <Output Address> <Amount in ADA> <Account Key file>
+Usage:  $(basename $0) <Tx-File to Create for submission> <Output Address> <Amount in ADA> <Signing Key file (script expects .vkey with same name in same folder)>
 EOF
   exit 1;; esac
 
@@ -26,45 +36,55 @@ echo "NW Magic is $NWMAGIC"
 
 echo "getting protocol params"
 ${CCLI} shelley query protocol-parameters --testnet-magic ${NWMAGIC} > /tmp/protparams.json
+# cat /tmp/protparams.json
+
+currSlot=`cardano-cli shelley query tip --testnet-magic 42 | awk '{ print $5 }' | grep -Eo '[0-9]{1,}'`
+ttlvalue=$(($currSlot+1000))
+echo "current slot is $currSlot, setting ttl to $ttlvalue"
 
 echo "calculating min fee"
-MINFEE=`${CCLI} shelley transaction calculate-min-fee --tx-in-count 1 --tx-out-count 2 --ttl 100000 --testnet-magic ${NWMAGIC} --signing-key-file ${from_key} --protocol-params-file /tmp/protparams.json | awk '{ print $2 }'`
+MINFEE=`${CCLI} shelley transaction calculate-min-fee --tx-in-count 1 --tx-out-count 2 --ttl ${ttlvalue} --testnet-magic ${NWMAGIC} --signing-key-file ${from_key} --protocol-params-file /tmp/protparams.json | awk '{ print $2 }'`
 echo "min fee is $MINFEE"
 
 from_addr=`${CCLI} shelley address build-enterprise --payment-verification-key-file "${from_key%.*}.vkey"`
 echo "from_ddress is $from_addr"
 
 echo "balance check.."
-${CCLI} shelley query filtered-utxo --testnet-magic ${NWMAGIC} --address ${from_addr} > /tmp/balance.txt
+${CCLI} shelley query filtered-utxo --testnet-magic ${NWMAGIC} --address ${from_addr} > /tmp/fullUtxo.out
+${CCLI} shelley query filtered-utxo --testnet-magic ${NWMAGIC} --address ${from_addr} | grep -v TxHash | grep -v "\-" | sort -k 3,3 -V | head -n 1  > /tmp/balance.txt
 cat /tmp/balance.txt
 
-
-if [[ `cat /tmp/balance.txt | wc -l` -lt 3 ]]; then
-	echo "Aborting as balance check yielded under 3 lines"
-	cleanupAndExit
+if [ ! -s /tmp/balance.txt ]; then
+	myexit "Aborting, as failed to locate a UTXO"
 fi
 
-inaddr=`tail -n 1 /tmp/balance.txt | awk '{ print $1 }'`
-idx=`tail -n 1 /tmp/balance.txt | awk '{ print $2 }'`
-origbalance=`tail -n 1 /tmp/balance.txt | awk '{ print $3 }'`
+inaddr=`awk '{ print $1 }' /tmp/balance.txt`
+idx=`awk '{ print $2 }' /tmp/balance.txt`
+origbalance=`awk '{ print $3 }' /tmp/balance.txt`
+
+echo "Using UTXO with highest value balance:"
+cat /tmp/balance.txt
 
 newbalance=$(($origbalance-$MINFEE-$lovelace))
-echo "new balance would be $newbalance"
+echo "new balance would be $newbalance lovelaces ($origbalance minus $MINFEE minus $lovelace)"
 
-echo "Going to try sending $3 ada from $inaddr index $idx"
+if [ $newbalance -lt 0 ]; then
+	myexit "New balance is $newbalance is negative - aborting"
+fi
 
+echo "Going to try sending $3 ADA from $inaddr index $idx"
 
 #TODO : Update fee and ttl dynamically
 args=" shelley transaction build-raw 
   --tx-in               ${inaddr}#${idx}
   --tx-out              ${outaddr}+$lovelace
   --tx-out		${from_addr}+$newbalance
-  --ttl                 100000
+  --ttl                 ${ttlvalue}
   --fee                 ${MINFEE}
   --tx-body-file        ${tx}
 "
 
-echo "Args value is: $args"
+# echo "Args value is: $args"
 
 NETARGS=(
   shelley transaction submit
@@ -76,22 +96,19 @@ set -x
 
 ${CCLI} ${args}
 if [ $? -ne 0 ]; then
-	echo "1 Something went wrong with args ${args}"
-	cleanupAndExit
+	myexit  "1. Problem during tx creation with args ${args}"
 fi
 
 ${CCLI} shelley transaction sign --tx-body-file ${tx} --signing-key-file ${from_key} --testnet-magic ${NWMAGIC} --tx-file /tmp/tx.signed
 if [ $? -ne 0 ]; then
-	echo "1.5 Something went wrong during signing"
-	cleanupAndExit
+	myexit "1.5 Problem during signing"
 fi
 
 echo "Starting sending"
 
 ${CCLI} ${NETARGS[*]}
 if [ $? -ne 0 ]; then
-	echo "2 Something went wrong with ${NETARGS}"
-	cleanupAndExit
+	myexit "2. Problem during tx submission with args ${NETARGS}"
 fi
 
 echo "Finished"
