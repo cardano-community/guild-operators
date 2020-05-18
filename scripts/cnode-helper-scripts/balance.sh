2b537edb6ca2f901e28506471fd6d485f9616103
Priyank <3169068+rdlrt@users.noreply.github.com>
2020-05-18 20:12:41 +1000

    Update balance.sh for shlint and file check
    Need to check file instead of digit as addresses on master are not 72 chars long

--- scripts/cnode-helper-scripts/balance.sh
+++ scripts/cnode-helper-scripts/balance.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
+# shellcheck disable=SC1090
 # Only source env if not done already, this script is sourced from other scripts
-[ "$0" = "$BASH_SOURCE" ] && . $(dirname $0)/env
+[ "$0" = "${BASH_SOURCE[*]}" ] && . "$(dirname "$0")"/env
 
-usage() { echo "Usage: $(basename $0) <address or path to address file>" 1>&2; exit 1; }
+usage() { echo "Usage: $(basename "$0") <address or path to address file>" 1>&2; exit 1; }
+
 if [[ $# -eq 0 ]]; then
   usage
-elif [[ $# -eq 1 && ${#1} -ge 72 ]]; then
+elif [[ $# -eq 1 && ! -f "${#1}" ]]; then
   WALLET_ADDR=$1
 elif [[ $# -eq 1 ]]; then
-  WALLET_ADDR="$(cat $1)"
+  WALLET_ADDR="$(cat "$1")"
 else
   usage
 fi
@@ -23,26 +24,26 @@ function cleanup() {
 cleanup
 
 # The "testnet magic" is specific on the testnet and will not be needed for the mainnet.
-${CCLI} shelley query filtered-utxo --testnet-magic ${NWMAGIC} --address ${WALLET_ADDR} > /tmp/fullUtxo.out
+${CCLI} shelley query filtered-utxo --testnet-magic "${NWMAGIC}" --address "${WALLET_ADDR}" > /tmp/fullUtxo.out
 echo ""
 head -n 2 /tmp/fullUtxo.out
 
-cat /tmp/fullUtxo.out | grep -v TxHash | grep -v "\-" | sort -k3 -nr > /tmp/balance.txt
+grep -v TxHash < /tmp/fullUtxo.out | grep -v "\-" | sort -k3 -nr > /tmp/balance.txt
 head -n 10 /tmp/balance.txt
 if [ ! -s /tmp/balance.txt ]; then
-  [ "$0" = "$BASH_SOURCE" ] && exit || return
+  [ "$0" = "${BASH_SOURCE[*]}" ] && exit || return
 fi
 
 TOTALBALANCE=0
 UTx0_COUNT=0
-while read UTxO; do
+while read -r UTxO; do
   INADDR=$(awk '{ print $1 }' <<< "$UTxO")
   IDX=$(awk '{ print $2 }' <<< "$UTxO")
   BALANCE=$(awk '{ print $3 }' <<< "$UTxO")
-  
-  UTx0_COUNT=$((${UTx0_COUNT}+1))
+
+  UTx0_COUNT=$(( UTx0_COUNT + 1 ))
   TX_IN="${TX_IN} --tx-in ${INADDR}#${IDX}"
-  TOTALBALANCE=$((${TOTALBALANCE}+${BALANCE}))
+  TOTALBALANCE=$(( TOTALBALANCE + BALANCE ))
 done </tmp/balance.txt
 
 [[ ${UTx0_COUNT} -gt 10 ]] && echo "... (top 10 UTx0 with most lovelace)"
@@ -50,6 +51,7 @@ done </tmp/balance.txt
 # ADA pretty print explanation for sed
 # remove trailing 0 IF there is a decimal separator
 # remove the separator if there are only 0 after separator also (assuming there is at least a digit before like BC does)
+
 TOTALBALANCE_ADA=$(echo "${TOTALBALANCE}/1000000" | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
 echo ""
 echo "Total balance in ${UTx0_COUNT} UTxO is ${TOTALBALANCE} Lovelace or ${TOTALBALANCE_ADA} ADA"
