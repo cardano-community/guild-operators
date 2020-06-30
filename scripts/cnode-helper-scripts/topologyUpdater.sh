ad43d4d963727b9c813cf9654fb1c9ee28b92190
rdlrt <3169068+rdlrt@users.noreply.github.com>
2020-06-30 02:54:23 +0000

    topologyUpdater.sh: Setting PATH trips some users, some other cleanup

--- scripts/cnode-helper-scripts/topologyUpdater.sh
+++ scripts/cnode-helper-scripts/topologyUpdater.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-USERNAME="nonroot" # replace nonroot with your username
+USERNAME="${USERNAME}" # replace nonroot with your username
 
-CNODE_BIN="/home/${USERNAME}/.cabal/bin"
+CNODE_BIN="${HOME}/.cabal/bin"
 CNODE_HOME="/opt/cardano/cnode"
 CNODE_LOG_DIR="${CNODE_HOME}/logs/"
 
 CNODE_PORT=6000  # must match your relay node port as set in the startup command
-CNODE_HOSTNAME="myrelays.mydomain.com"  # optional. must resolve to the IP you are requesting from
+CNODE_HOSTNAME="$(curl -s ifconfig.me)"  # optional. must resolve to the IP you are requesting from
 CNODE_VALENCY=1   # optional for multi-IP hostnames
 
 TESTNET_MAGIC=42
 
-
-export DISPLAY=":0"
-export PATH="${CNODE_BIN}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export SHELL="/bin/bash"
+export PATH="${CNODE_BIN}:${PATH}"
 export CARDANO_NODE_SOCKET_PATH="${CNODE_HOME}/sockets/node0.socket"
 
 blockNo=$(cardano-cli shelley query tip --testnet-magic $TESTNET_MAGIC | grep -oP 'unBlockNo = \K\d+')
