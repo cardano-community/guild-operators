a9a2c5aede9b89095f10b1ed51017336b8ec1d83
Priyank <3169068+rdlrt@users.noreply.github.com>
2020-07-01 15:55:42 +1000

    Update topologyUpdater.sh
    Skip Hostname parameter if not specified: Since it is optional, and some miss to read instructions

--- scripts/cnode-helper-scripts/topologyUpdater.sh
+++ scripts/cnode-helper-scripts/topologyUpdater.sh
@@ -7,7 +7,7 @@ CNODE_HOME="/opt/cardano/cnode"
 CNODE_LOG_DIR="${CNODE_HOME}/logs/"
 
 CNODE_PORT=6000  # must match your relay node port as set in the startup command
-CNODE_HOSTNAME="$(curl -s ifconfig.me)"  # optional. must resolve to the IP you are requesting from
+CNODE_HOSTNAME=""  # optional. must resolve to the IP you are requesting from
 CNODE_VALENCY=1   # optional for multi-IP hostnames
 
 TESTNET_MAGIC=42
@@ -20,5 +20,10 @@ blockNo=$(cardano-cli shelley query tip --testnet-magic $TESTNET_MAGIC | grep -o
 # Note: 
 # if you run your node in IPv4/IPv6 dual stack network configuration and want announced the 
 # IPv4 address only please add the -4 parameter to the curl command below  (curl -4 -s ...)
+if [ "${CNODE_HOSTNAME}" != "CHANGE ME" ]; then
+  T_HOSTNAME='&hostname=${CNODE_HOSTNAME}'
+else
+  T_HOSTNAME=''
+fi
 
-curl -s "https://api.clio.one/htopology/v1/?port=${CNODE_PORT}&blockNo=${blockNo}&hostname=${CNODE_HOSTNAME}&valency=${CNODE_VALENCY}&magic=${TESTNET_MAGIC}" | tee -a $CNODE_LOG_DIR/topologyUpdater_lastresult.json
+curl -s "https://api.clio.one/htopology/v1/?port=${CNODE_PORT}&blockNo=${blockNo}&valency=${CNODE_VALENCY}&magic=${TESTNET_MAGIC}${T_HOSTNAME}" | tee -a $CNODE_LOG_DIR/topologyUpdater_lastresult.json
