a2ba24a4f62081a3b798c6bf0aee2a5118393723
rdlrt <3169068+rdlrt@users.noreply.github.com>
2020-07-21 00:15:59 +0000

    createAddr script was out of date, update script and doco, update shelley genesis

--- scripts/cnode-helper-scripts/createAddr.sh
+++ scripts/cnode-helper-scripts/createAddr.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # shellcheck disable=SC2086,SC1090
 . "$(dirname $0)"/env
 
 if  [ "$1" = "--help" ] || [ $# -ne 1 ]; then
-    echo "usage: $0 <Name of Key File>"
-    exit 1
+  echo "Usage: $0 <Path with name (prefix) for keys to be created>"
+  exit 1
 fi
 
-WNAME=$1
-${CCLI} shelley address key-gen --verification-key-file $WNAME.vkey --signing-key-file $WNAME.skey
-${CCLI} shelley address build --payment-verification-key-file $WNAME.vkey  ${NETWORK_IDENTIFIER} | tee $WNAME.addr
+WNAME="$1"
+${CCLI} shelley address key-gen --verification-key-file ${WNAME}_pay.vkey --signing-key-file ${WNAME}_pay.skey
+${CCLI} shelley stake-address key-gen --verification-key-file ${WNAME}_stake.vkey --signing-key-file ${WNAME}_stake.skey
+${CCLI} shelley address build --payment-verification-key-file ${WNAME}_pay.vkey  ${HASH_IDENTIFIER} | tee ${WNAME}_pay.addr
+${CCLI} shelley address build --payment-verification-key-file ${WNAME}_pay.vkey --stake-verification-key-file ${WNAME}_stake.vkey ${HASH_IDENTIFIER} | tee ${WNAME}_stake.addr
