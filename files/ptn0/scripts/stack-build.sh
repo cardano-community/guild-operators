0c3dad8bfd4203d20c2c1b5222180d4053d291d9
rdlrt <3169068+rdlrt@users.noreply.github.com>
2020-07-26 10:21:16 +0000

    Check if stack is unavailable and install it if missing

--- files/ptn0/scripts/stack-build.sh
+++ files/ptn0/scripts/stack-build.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+# Install stack if not installed on system
+which stack > /dev/null 2>&1
+rc=$?
+[[ "$rc" != "0" ]] && curl -sSL https://get.haskellstack.org/ | sh
 
 # executes stack build and copies binaries to ~/.cabal/bin folder.
-
 stack build --test --no-run-tests --copy-bins --local-bin-path ~/.cabal/bin  2>&1 | tee /tmp/build.log
