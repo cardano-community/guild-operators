ecdfcad473b28d21e9b2e45bf6a50114aaf6093c
rdlrt <3169068+rdlrt@users.noreply.github.com>
2020-09-14 03:26:14 +0000

    Change operator to =~

--- scripts/cnode-helper-scripts/prereqs.sh
+++ scripts/cnode-helper-scripts/prereqs.sh
@@ -127,7 +127,7 @@ if [ "$WANT_BUILD_DEPS" = 'Y' ]; then
       echo "It would be best if you could submit an issue at https://github.com/cardano-community/guild-operators with the details to tackle in future, as some errors may be due to external/already present dependencies"
       exit;
     fi
-  elif [ -z "${OS_ID##*rhel*}" ] || [[ "${DISTRO}" == *Fedora* ]]; then
+  elif [ -z "${OS_ID##*rhel*}" ] || [[ "${DISTRO}" =~ "Fedora" ]]; then
     #CentOS/RHEL/Fedora
     echo "Using yum to prepare packages for ${DISTRO} system"
     echo "  Updating system packages..."
@@ -135,7 +135,7 @@ if [ "$WANT_BUILD_DEPS" = 'Y' ]; then
     $sudo yum -y update > /dev/null
     echo "  Installing missing prerequisite packages, if any.."
     pkg_list="python3 coreutils pkgconfig libffi-devel gmp-devel openssl-devel ncurses-libs ncurses-compat-libs systemd systemd-devel libsodium-devel zlib-devel make gcc-c++ tmux git wget jq gnupg libtool autoconf srm iproute bc tcptraceroute"
-    [[ ! "${DISTRO}" == *Fedora* ]] && pkg_list="epel-release $pkg_list"
+    [[ ! "${DISTRO}" =~ "Fedora" ]] && pkg_list="epel-release $pkg_list"
     $sudo yum -y install ${pkg_list} > /dev/null;rc=$?
     if [ $rc != 0 ]; then
       echo "An error occurred while installing the prerequisite packages, please investigate by using the command below:"
