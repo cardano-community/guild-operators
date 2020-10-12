0ab64b90a970dda540a82fc513758eb7b092813b
Priyank <3169068+rdlrt@users.noreply.github.com>
2020-10-13 00:42:35 +1100

    Make TopoUpdater updates and variable extraction uniform with repo (#526)
    * Align env retrieval as per guild repo, while keeping flexibility for script to be guild repo independent
    
    * Update topology Updater doco
    
    * Source env file after update
    
    * Replace cardano-cli with ${CCLI}
    
    * Add fetch request after a successful POST; Update doco
    
    * Replace CCLI call by EKG one

--- scripts/cnode-helper-scripts/topologyUpdater.sh
+++ scripts/cnode-helper-scripts/topologyUpdater.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
 # shellcheck disable=SC2086,SC2034
+# shellcheck source=/dev/null
 
 ######################################
 # User Variables - Change as desired #
 ######################################
 
-CNODE_PORT=6000                                           # Must match your relay node port as set in the startup command
 CNODE_HOSTNAME="CHANGE ME"                                # (Optional) Must resolve to the IP you are requesting from
-CNODE_BIN="${HOME}/.cabal/bin"                            # Path where your cardano-cli and cardano-node binaries are
-CNODE_HOME="/opt/cardano/cnode"                           # (Optional) Top-level folder to auto populate file locations under, useful if using guild repo instructions
 CNODE_LOG_DIR="${CNODE_HOME}/logs/"                       # Folder where your logs will be sent to (must pre-exist)
-CONFIG="$CNODE_HOME/files/config.json"                    # Filename with path for config used by node
-GENESIS_JSON=$(jq -er '.ShelleyGenesisFile' "${CONFIG}")  # Filename with path for Shelley genesis file used by node (auto detected if your config is in JSON format)
-SOCKET="${CNODE_HOME}/sockets/node0.socket"               # Path to socket file for your cardano node instance
 CNODE_VALENCY=1                                           # (Optional) for multi-IP hostnames
+CNODE_TOPOLOGY="${CNODE_HOME}/files/topology.json"        # Destination topology.json file you'd want to write output to
+MAX_PEERS=15                                              # Maximum number of peers to return on successful fetch
+CUSTOM_PEERS="None"                                       # Additional custom peers to (IP:port[:valency]) to add to your target topology.json, eg: "10.0.0.1:3001|10.0.0.2:3002|relays.mydomain.com:3003:3"
 
 ######################################
 # Do NOT modify code below           #
 ######################################
 
-NETWORKID=$(jq -r .networkId $GENESIS_JSON 2>/dev/null)
-PROTOCOL=$(grep -E '^.{0,1}Protocol.{0,1}:' "$CONFIG" | tr -d '"' | tr -d ',' | awk '{print $2}')
-[[ "${PROTOCOL}" = "Cardano" ]] && PROTOCOL_IDENTIFIER="--cardano-mode"
-CNODE_VALENCY=1   # optional for multi-IP hostnames
-NWMAGIC=$(jq -r .networkMagic < $GENESIS_JSON)
-[[ "${NETWORKID}" = "Mainnet" ]] && HASH_IDENTIFIER="--mainnet" || HASH_IDENTIFIER="--testnet-magic ${NWMAGIC}"
-[[ "${NWMAGIC}" = "764824073" ]] && NETWORK_IDENTIFIER="--mainnet" || NETWORK_IDENTIFIER="--testnet-magic ${NWMAGIC}"
+PARENT="$(dirname $0)"
+BRANCH="master"
 
-export PATH="${CNODE_BIN}:${PATH}"
-export CARDANO_NODE_SOCKET_PATH="${SOCKET}"
+usage() {
+  cat <<EOF
+Usage: $(basename "$0") [-a]
+Topology Updater - Build topology with community pools
 
-blockNo=$(cardano-cli shelley query tip ${PROTOCOL_IDENTIFIER} ${NETWORK_IDENTIFIER} | jq -r .blockNo )
+-a    Use alpha branch to check for updates - only for testing/development
+
+EOF
+  exit 1
+}
+
+while getopts :a opt; do
+  case ${opt} in
+    a ) BRANCH="alpha" ;;
+    \? ) usage ;;
+    esac
+done
+shift $((OPTIND -1))
+
+URL="https://raw.githubusercontent.com/cardano-community/guild-operators/${BRANCH}/scripts/cnode-helper-scripts"
+curl -s -m 10 -o "${PARENT}"/env.tmp ${URL}/env
+if [[ -f "${PARENT}"/env ]]; then
+  TEMPL_CMD=$(awk '/^# Do NOT modify/,0' "${PARENT}"/env)
+  TEMPL2_CMD=$(awk '/^# Do NOT modify/,0' "${PARENT}"/env.tmp)
+  if [[ "$(echo ${TEMPL_CMD} | sha256sum)" != "$(echo ${TEMPL2_CMD} | sha256sum)" ]]; then
+    cp "${PARENT}"/env "${PARENT}/env.bkp_$(date +%s)"
+    STATIC_CMD=$(awk '/#!/{x=1}/^# Do NOT modify/{exit} x' "${PARENT}"/env)
+    printf '%s\n%s\n' "$STATIC_CMD" "$TEMPL2_CMD" > "${PARENT}"/env.tmp
+    mv "${PARENT}"/env.tmp "${PARENT}"/env
+  fi
+else
+  mv "${PARENT}"/env.tmp "${PARENT}"/env
+fi
+rm -f "${PARENT}"/env.tmp
+
+# source common env variables in case it was updated
+if ! . "${PARENT}"/env; then exit 1; fi
+
+blockNo=$(curl -s -m ${EKG_TIMEOUT} -H 'Accept: application/json' "http://${EKG_HOST}:${EKG_PORT}/" 2>/dev/null | jq '.cardano.node.ChainDB.metrics.blockNum.int.val //0' )
 
 # Note: 
 # if you run your node in IPv4/IPv6 dual stack network configuration and want announced the 
@@ -41,4 +69,10 @@ else
   T_HOSTNAME=''
 fi
 
-curl -s "https://api.clio.one/htopology/v1/?port=${CNODE_PORT}&blockNo=${blockNo}&valency=${CNODE_VALENCY}&magic=${NWMAGIC}${T_HOSTNAME}" | tee -a $CNODE_LOG_DIR/topologyUpdater_lastresult.json
+[[ "${CUSTOM_PEERS}" = "None" ]] && CUSTOM_PEERS_PARAM="" || CUSTOM_PEERS_PARAM="&customPeers=${CUSTOM_PEERS}"
+
+curl -s "https://api.clio.one/htopology/v1/?port=${CNODE_PORT}&blockNo=${blockNo}&valency=${CNODE_VALENCY}&magic=${NWMAGIC}${T_HOSTNAME}" | tee -a $CNODE_LOG_DIR/topologyUpdater_lastresult.json && \
+curl -s -o "${CNODE_TOPOLOGY}".tmp "https://api.clio.one/htopology/v1/fetch/?max=${MAX_PEERS}&magic=${NWMAGIC}${CUSTOM_PEERS_PARAM}" && \
+mv "${CNODE_TOPOLOGY}".tmp "${CNODE_TOPOLOGY}"
+
+
