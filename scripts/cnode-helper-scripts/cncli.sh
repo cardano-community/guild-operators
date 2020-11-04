dc94a69945bae78bea73cda04386298b7d0cd284
Ola <ola.ahlman@gmail.com>
2020-11-05 00:09:19 +0100

    Add IOG libsodium fork to LD_LIBRARY_PATH in cncli.sh

--- scripts/cnode-helper-scripts/cncli.sh
+++ scripts/cnode-helper-scripts/cncli.sh
@@ -11,12 +11,15 @@
 ######################################
 
 #CNCLI_DB="${CNODE_HOME}/db/cncli"        # path to sqlite db for cncli
+#LIBSODIUM_FORK=/usr/local/lib            # path to IOG fork of libsodium
 
 ######################################
 # Do NOT modify code below           #
 ######################################
 
 [[ -z "${CNCLI_DB}" ]] && CNCLI_DB="${CNODE_HOME}/db/cncli"
+[[ -z "${LIBSODIUM_FORK}" ]] && LIBSODIUM_FORK=/usr/local/lib
+export LD_LIBRARY_PATH="${LIBSODIUM_FORK}:${LD_LIBRARY_PATH}"
 
 [[ ! -f "${CNCLI}" ]] && echo "failed to locate cncli executable, please update to latest env file and run install-cncli.sh!" && exit 1
 
