e50fa52c94b5fa83f4a44ef94909d922a66a11cc
Ola <ola.ahlman@gmail.com>
2020-11-09 23:04:26 +0100

    clean exit in prereqs.sh

--- scripts/cnode-helper-scripts/prereqs.sh
+++ scripts/cnode-helper-scripts/prereqs.sh
@@ -337,4 +337,4 @@ updateWithCustomConfig "cncli.sh"
 chmod -R 755 "${CNODE_HOME}"
 chmod -R 700 "${CNODE_HOME}"/priv 2>/dev/null
 
-pushd -0 >/dev/null && dirs -c || err_exit
+pushd -0 >/dev/null && dirs -c || return
