5ea9e8f2af30565cc1308f4b3274971b6a2e631d
rdlrt <3169068+rdlrt@users.noreply.github.com>
2020-11-23 03:43:23 +0000

    Add 1.23 as a baseline; and replace uptimens using new metrics

--- scripts/cnode-helper-scripts/gLiveView.sh
+++ scripts/cnode-helper-scripts/gLiveView.sh
@@ -52,6 +52,8 @@ setTheme() {
 
 GLV_VERSION=v1.12
 
+[[ $(cardano-node version | head -1 | awk '{print $2}' | cut -d. -f 2) -le 22 ]] && echo -e "\ngLiveView has now been upgraded to support cardano-node 1.23 or higher. Please update cardano-node or us node-1.21 branch for gLiveView\n\n" && exit 1
+
 PARENT="$(dirname $0)"
 [[ -f "${PARENT}"/.env_branch ]] && BRANCH="$(cat ${PARENT}/.env_branch)" || BRANCH="master"
 
@@ -522,7 +524,8 @@ while true; do
   node_version=$(grep "cardano-node" <<< "${version}" | cut -d ' ' -f2)
   node_rev=$(grep "git rev" <<< "${version}" | cut -d ' ' -f3 | cut -c1-8)
   data=$(curl -s -m ${EKG_TIMEOUT} -H 'Accept: application/json' "http://${EKG_HOST}:${EKG_PORT}/" 2>/dev/null)
-  uptimens=$(jq '.cardano.node.metrics.upTime.ns.val //0' <<< "${data}")
+  uptimens=$(jq '.rts.gc.wall_ms.val //0' <<< "${data}")
+  #uptimens=$(jq '.cardano.node.metrics.upTime.ns.val //0' <<< "${data}")
   [[ ${fail_count} -eq ${RETRIES} ]] && myExit 1 "${style_status_3}COULD NOT CONNECT TO A RUNNING INSTANCE, ${RETRIES} FAILED ATTEMPTS IN A ROW!${NC}"
   if [[ ${uptimens} -le 0 ]]; then
     ((fail_count++))
@@ -575,7 +578,7 @@ while true; do
 
   ## main section ##
   printf "${tdivider}\n" && ((line++))
-  printf "${VL} Uptime: ${style_values_1}%s${NC}" "$(timeLeft $(( uptimens/1000000000 )))"
+  printf "${VL} Uptime: ${style_values_1}%s${NC}" "$(timeLeft $(( uptimens/1000 )))"
   tput cup ${line} $(( width - ${#title} - 3 ))
   printf "${VL} ${style_title}${title} ${VL}\n" && ((line++))
   printf "${m2divider}"
