b3a1da2820c6d43638eb9aec472bc955a6e5c89e
rdlrt <3169068+rdlrt@users.noreply.github.com>
2021-02-03 08:34:08 +0100

    Fix sendADA script for changes to function names

--- scripts/cnode-helper-scripts/sendADA.sh
+++ scripts/cnode-helper-scripts/sendADA.sh
@@ -77,10 +77,10 @@ fi
 
 amount_lovelace="$2"
 if [[ ${amount_lovelace} != "all" ]]; then
-  if ! ADAtoLovelace "${amount_lovelace}" >/dev/null; then
+  if ! AdaToLovelace "${amount_lovelace}" >/dev/null; then
     echo && exit 1
   fi
-  amount_lovelace=$(ADAtoLovelace "${amount_lovelace}")
+  amount_lovelace=$(AdaToLovelace "${amount_lovelace}")
   if [[ ${amount_lovelace} -gt ${lovelace} ]]; then
     echo -e "${RED}ERROR${NC}: not enough funds available in source address"
     echo && exit 1
@@ -91,7 +91,9 @@ else
   include_fee="yes"
 fi
 
-if ! sendADA; then
+s_payment_sk_file="${payment_sk_file}"
+
+if ! sendAda; then
   echo && exit 1
 fi
 
