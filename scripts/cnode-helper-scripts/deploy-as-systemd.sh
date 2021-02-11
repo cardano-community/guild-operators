49766ac48ea5eb3d0a46fe9fb1894f305d8135c6
Ola <ola.ahlman@gmail.com>
2021-02-11 19:01:55 +0100

    Reduce scheduling priority of cncli/logmonitor systemd services

--- scripts/cnode-helper-scripts/deploy-as-systemd.sh
+++ scripts/cnode-helper-scripts/deploy-as-systemd.sh
@@ -12,6 +12,8 @@ fi
 
 . "${PARENT}"/env offline
 
+if nice_cmd="$(command -v nice)"; then nice_cmd+=" -n 19"; else nice_cmd=""; fi
+
 echo -e "\e[32m~~ Cardano Node ~~\e[0m"
 echo "launches the main cnode.sh script to start cardano-node"
 echo
@@ -171,7 +173,7 @@ Restart=on-failure
 RestartSec=20
 User=$USER
 WorkingDirectory=${CNODE_HOME}/scripts
-ExecStart=/bin/bash -l -c \"exec ${CNODE_HOME}/scripts/cncli.sh sync\"
+ExecStart=/bin/bash -l -c \"exec ${nice_cmd} ${CNODE_HOME}/scripts/cncli.sh sync\"
 ExecStop=/bin/bash -l -c \"exec kill -2 \$(ps -ef | grep [c]ncli.sync.*.${CNODE_HOME}/ | tr -s ' ' | cut -d ' ' -f2) &>/dev/null\"
 KillSignal=SIGINT
 SuccessExitStatus=143
@@ -195,7 +197,7 @@ Restart=on-failure
 RestartSec=20
 User=$USER
 WorkingDirectory=${CNODE_HOME}/scripts
-ExecStart=/bin/bash -l -c \"exec ${CNODE_HOME}/scripts/cncli.sh leaderlog\"
+ExecStart=/bin/bash -l -c \"exec ${nice_cmd} ${CNODE_HOME}/scripts/cncli.sh leaderlog\"
 SuccessExitStatus=143
 StandardOutput=syslog
 StandardError=syslog
@@ -219,7 +221,7 @@ RestartSec=20
 User=$USER
 WorkingDirectory=${CNODE_HOME}/scripts
 ExecStartPre=/bin/sleep 5
-ExecStart=/bin/bash -l -c \"exec ${CNODE_HOME}/scripts/cncli.sh validate\"
+ExecStart=/bin/bash -l -c \"exec ${nice_cmd} ${CNODE_HOME}/scripts/cncli.sh validate\"
 SuccessExitStatus=143
 StandardOutput=syslog
 StandardError=syslog
@@ -250,7 +252,7 @@ Restart=on-failure
 RestartSec=20
 User=$USER
 WorkingDirectory=${CNODE_HOME}/scripts
-ExecStart=/bin/bash -l -c \"exec ${CNODE_HOME}/scripts/cncli.sh ptsendslots\"
+ExecStart=/bin/bash -l -c \"exec ${nice_cmd} ${CNODE_HOME}/scripts/cncli.sh ptsendslots\"
 ExecStop=/bin/bash -l -c \"exec kill -2 \$(ps -ef | grep [c]ncli.sendslots.*.${vname}-pooltool.json | tr -s ' ' | cut -d ' ' -f2) &>/dev/null\"
 KillSignal=SIGINT
 SuccessExitStatus=143
@@ -292,7 +294,7 @@ Restart=on-failure
 RestartSec=20
 User=$USER
 WorkingDirectory=${CNODE_HOME}/scripts
-ExecStart=/bin/bash -l -c \"exec ${CNODE_HOME}/scripts/logMonitor.sh\"
+ExecStart=/bin/bash -l -c \"exec ${nice_cmd} ${CNODE_HOME}/scripts/logMonitor.sh\"
 ExecStop=/bin/bash -l -c \"exec kill -2 \$(ps -ef | grep -m1 ${CNODE_HOME}/scripts/logMonitor.sh | tr -s ' ' | cut -d ' ' -f2) &>/dev/null\"
 KillSignal=SIGINT
 SuccessExitStatus=143
@@ -359,7 +361,7 @@ Restart=on-failure
 RestartSec=20
 User=$USER
 WorkingDirectory=${CNODE_HOME}/scripts
-ExecStart=/bin/bash -l -c \"exec ${CNODE_HOME}/scripts/cncli.sh ptsendtip\"
+ExecStart=/bin/bash -l -c \"exec ${nice_cmd} ${CNODE_HOME}/scripts/cncli.sh ptsendtip\"
 ExecStop=/bin/bash -l -c \"exec kill -2 \$(ps -ef | grep [c]ncli.sendtip.*.${vname}-pooltool.json | tr -s ' ' | cut -d ' ' -f2) &>/dev/null\"
 KillSignal=SIGINT
 SuccessExitStatus=143
