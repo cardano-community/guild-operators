37cc1da070cb0ce5df179c125e5914bed855d775
Michael <michael.dogali@gmail.com>
2021-03-28 18:06:13 +0200

    entrypoint updated (#864)
    Co-authored-by: Michael <michael.dogai@gmail.com>

--- files/docker/node/addons/entrypoint.sh
+++ files/docker/node/addons/entrypoint.sh
@@ -8,7 +8,7 @@ head -n 8 ~/.scripts/banner.txt
 
 . ~/.bashrc > /dev/null 2>&1
 
-echo "NETWORK: $NETWORK $POOL_NAME";
+echo "NETWORK: $NETWORK $POOL_NAME $TOPOLOGY";
 
 [[ -z "${CNODE_HOME}" ]] && export CNODE_HOME=/opt/cardano/cnode 
 [[ -z "${CNODE_PORT}" ]] && export CNODE_PORT=6000
@@ -21,6 +21,10 @@ bksizedb=$(du -s $CNODE_HOME/priv/$NETWORK-db 2>/dev/null | awk '{print $1}')
 
 if [[ "$dbsize" -lt "$bksizedb" ]]; then
 cp -rf $CNODE_HOME/priv/$NETWORK-db/* ${CNODE_HOME}/db 2>/dev/null
+fi
+
+if [[ "$dbsize" -gt "$bksizedb" ]]; then
+cp -rf $CNODE_HOME/db/* $CNODE_HOME/priv/$NETWORK-db/ 2>/dev/null
 fi
 
 # EKG Exposed
@@ -42,6 +46,10 @@ elif [[ "$NETWORK" == "staging" ]]; then
 elif [[ "$NETWORK" == "guild-mainnet" ]]; then
   $CNODE_HOME/scripts/prereqs.sh -n mainnet -t cnode -s -f > /dev/null 2>&1 \
   && bash /home/guild/.scripts/guild-topology.sh > /dev/null 2>&1 \
+  && export TOPOLOGY="${CNODE_HOME}/files/guildnet-topology.json" \
+  && find /opt/cardano/cnode -name "*config*.json" -print0 | xargs -0 sed -i 's/127.0.0.1/0.0.0.0/g' > /dev/null 2>&1 \
+  && find /opt/cardano/cnode -name "*config*.json" -print0 | xargs -0 sed -i 's/\"TraceMempool\": true/\"TraceMempool\": false/g' > /dev/null 2>&1 \
+  && find /opt/cardano/cnode -name "cntools.config" -print0 | xargs -0 sed -i 's/ENABLE_CHATTR=true/ENABLE_CHATTR=false/g' > /dev/null 2>&1 \
   && exec $CNODE_HOME/scripts/cnode.sh
 elif [[ "$NETWORK" == "guild" ]]; then
   $CNODE_HOME/scripts/prereqs.sh -n guild -t cnode -s -f > /dev/null 2>&1 \
@@ -52,7 +60,3 @@ else
   echo "for active nodes set POOL_DIR environment variable where op.cert, hot.skey and vrf.skey files reside. (usually under '${CNODE_HOME}/priv/pool/$POOL_NAME' ) "
   echo "or just set POOL_NAME environment variable (for default path). "
 fi
-
-find /opt/cardano/cnode -name "*config*.json" -print0 | xargs -0 sed -i 's/127.0.0.1/0.0.0.0/g' 2> /dev/null 
-find /opt/cardano/cnode -name "*config*.json" -print0 | xargs -0 sed -i 's/\"TraceMempool\": true/\"TraceMempool\": false/g' 2> /dev/null 
-find /opt/cardano/cnode -name "cntools.config" -print0 | xargs -0 sed -i 's/ENABLE_CHATTR=true/ENABLE_CHATTR=false/g' 2> /dev/null 
