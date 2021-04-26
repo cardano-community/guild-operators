65af5f414d7fb10747e3f6ae58ddc0891aaec084
rdlrt <3169068+rdlrt@users.noreply.github.com>
2021-04-26 01:48:47 +0000

    Bump gLV and remove stale monitoring guide

--- scripts/cnode-helper-scripts/gLiveView.sh
+++ scripts/cnode-helper-scripts/gLiveView.sh
@@ -54,7 +54,7 @@ setTheme() {
 # Do NOT modify code below           #
 ######################################
 
-GLV_VERSION=v1.20.5
+GLV_VERSION=v1.20.6
 
 PARENT="$(dirname $0)"
 [[ -f "${PARENT}"/.env_branch ]] && BRANCH="$(cat ${PARENT}/.env_branch)" || BRANCH="master"
