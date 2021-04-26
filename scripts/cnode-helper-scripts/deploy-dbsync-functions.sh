7b0e170d237cb7b9a22af2a075ce19a0459bf452
Ola <ola.ahlman@gmail.com>
2021-04-26 22:34:43 +0200

    updated rpc functions

--- scripts/cnode-helper-scripts/deploy-dbsync-functions.sh
+++ scripts/cnode-helper-scripts/deploy-dbsync-functions.sh
@@ -23,7 +23,7 @@ if ! dbsync_network=$(psql -qtAX -d cexplorer -c "select network_name from meta;
 fi
 echo -e "Successfully connected to ${FG_LBLUE}${dbsync_network}${NC} Cardano DBSync PostgreSQL DB!"
 
-echo -e "\nDownloading DBSync RPC functions from Guild Operators GitHub store .."
+echo -e "\nDownloading DBSync RPC functions from Guild Operators GitHub store ..\n"
 if ! rpc_file_list=$(curl -s -m ${CURL_TIMEOUT} https://api.github.com/repos/cardano-community/guild-operators/contents/files/dbsync/rpc?ref=${BRANCH}); then
   echo -e "${FG_RED}ERROR${NC}: ${rpc_file_list}" && exit 1
 fi
@@ -36,17 +36,17 @@ for row in $(jq -r '.[] | @base64' <<< ${rpc_file_list}); do
   file_name=$(jqDecode '.name' "${row}")
   dl_url=$(jqDecode '.download_url //empty' "${row}")
   [[ -z ${dl_url} ]] && continue
-  file_content=$(curl -s -f -m ${CURL_TIMEOUT} -o /dev/tty ${dl_url} 2>/dev/null)
-  echo -e "${FG_GREEN}$(jq -r '.function' <<< ${file_content})${NC}"
-  echo -e "${FG_LGRAY}$(jq -r '.description' <<< ${file_content})${NC}"
+  ! file_content=$(curl -s -f -m ${CURL_TIMEOUT} ${dl_url} 2>/dev/null) && echo -e "${FG_RED}ERROR${NC}: download failed: ${dl_url}" && exit 1
+  echo -e "Function:    ${FG_GREEN}$(jq -r '.function' <<< ${file_content})${NC}"
+  echo -e "Description: ${FG_LGRAY}$(jq -r '.description' <<< ${file_content})${NC}"
   for param in $(jq -r '.parameters[] | @base64' <<< ${file_content}); do
-    echo -e "${FG_LBLUE}$(jqDecode '.name' "${param}")${NC}: ${FG_LGRAY}$(jqDecode '.description' "${param}")${NC}"
+    echo -e "Parameter:   ${FG_LBLUE}$(jqDecode '.name' "${param}")${NC} => ${FG_LGRAY}$(jqDecode '.description' "${param}")${NC}"
   done
   for example in $(jq -r '.example[] | @base64' <<< ${file_content}); do
-    echo -e "Example ${FG_LGRAY}$(jqDecode '.type' "${example}")${NC} query: ${FG_LGRAY}$(jqDecode '.command' "${example}")${NC}"
+    echo -e "Example $(jqDecode '.type' "${example}") query: ${FG_LGRAY}$(jqDecode '.command' "${example}")${NC}"
   done
   ! output=$(jq -r '.sql | join("\n")' <<< ${file_content} | psql cexplorer 2>&1) && echo -e "${FG_RED}ERROR${NC}: ${output}" && exit 1
-  echo -e "Function from ${FG_LGRAY}${file_name}${NC} successfully added to DBSync!\n"
+  echo
 done
 
 echo -e "${FG_GREEN}All RPC functions successfully added to DBSync!${NC}"
