65686bd7cfc7366ba4defa0ce82441ec4a11ce8c
Ola <ola.ahlman@gmail.com>
2021-06-11 11:07:42 +0200

    Utilize new get_pool_opcert rpc for pool KES rotation

--- scripts/cnode-helper-scripts/cntools.sh
+++ scripts/cnode-helper-scripts/cntools.sh
@@ -2739,7 +2739,7 @@ function main {
               echo
               [[ ! $(ls -A "${POOL_FOLDER}" 2>/dev/null) ]] && println "${FG_YELLOW}No pools available!${NC}" && waitForInput && continue
               println DEBUG "# Select pool to rotate KES keys on"
-              if ! selectPool "all" "${POOL_COLDKEY_SK_FILENAME}" "${POOL_OPCERT_COUNTER_FILENAME}"; then # ${pool_name} populated by selectPool function
+              if ! selectPool "all" "${POOL_COLDKEY_SK_FILENAME}"; then # ${pool_name} populated by selectPool function
                 waitForInput && continue
               fi
               if ! rotatePoolKeys; then
