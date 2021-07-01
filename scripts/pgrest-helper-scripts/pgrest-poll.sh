3702804c146f1a46968eb7ab0c92d00eef66f7ca
rdlrt <3169068+rdlrt@users.noreply.github.com>
2021-07-01 10:30:02 +0200

    Remove dependency on env file, as well as jq

--- scripts/pgrest-helper-scripts/pgrest-poll.sh
+++ scripts/pgrest-helper-scripts/pgrest-poll.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-PARENT="$(dirname $0)"
-. "${PARENT}"/env offline
 function usage() {
   echo -e "\nUsage: $(basename "$0") <haproxy IP> <haproxy port> <server IP> <server port>\n"
   echo -e "Polling script used by haproxy to query server IP at server Port, and check that slot is almost on tip\n\n"
@@ -16,10 +14,11 @@ haproxy_ip="${1}"
 haproxy_port="${2}"
 server="${3}"
 port="${4}"
-dbtip=$(curl -f -s "http://${3}:${4}/block?select=slot_no&order=slot_no.desc.nullslast&limit=1" 2>/dev/null | jq -r .[0].slot_no)
-currtip=$(getSlotTipRef)
-if [[ -n "{dbtip}" ]] || [[ -n "${currtip}" ]]; then
-  [[ $(( currtip - dbtip )) -lt 120 ]] && exit 0 || exit 1
+dbtip=$(curl -f -H "Accept: text/plain" "http://127.0.0.1:8050/epoch?select=end_time::text&order=id.desc.nullslast&limit=1" 2>/dev/null)
+currtip=$(TZ='UTC' date "+%Y-%m-%d %H:%M:%S")
+echo ${dbtip} - ${currtip}
+if [[ -n "{dbtip}" ]] ; then
+  [[ $(( $(date -d "${currtip}" +%s) - $(date -d "${dbtip}" +%s) )) -lt 120 ]] && exit 0 || exit 2
 else
   exit 1
 fi
