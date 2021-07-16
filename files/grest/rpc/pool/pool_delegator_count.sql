fabbccd1558b667de785a1996230c2cd33b01d6e
Ola <ola.ahlman@gmail.com>
2021-07-16 12:07:44 +0200

    First draft for restructuring rpc and adding grest schema to dbsync
    CNTools not updated yet to reflect the changes.

---
+++ files/grest/rpc/pool/pool_delegator_count.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION grest.pool_delegator_count(_pool_bech32 text)
+RETURNS JSON STABLE LANGUAGE PLPGSQL AS $$
+BEGIN
+    RETURN ( SELECT json_build_object(
+        'delegator_count', COUNT(*)
+    )
+    FROM public.delegation d
+    WHERE pool_hash_id = (SELECT id from public.pool_hash where view=_pool_bech32)
+        AND NOT EXISTS
+        (SELECT TRUE FROM public.delegation d2 WHERE d2.addr_id=d.addr_id AND d2.id > d.id)
+        AND NOT EXISTS
+        (SELECT TRUE FROM public.stake_deregistration sd WHERE sd.addr_id=d.addr_id AND sd.tx_id > d.tx_id)
+    );
+END; $$;
