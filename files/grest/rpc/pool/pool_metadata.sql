21e9d2b78d7697f12f681a5ba439e9a4e4f8d2a5
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-07-27 18:48:01 +0100

    pool_metadata rpc update

--- files/grest/rpc/pool/pool_metadata.sql
+++ files/grest/rpc/pool/pool_metadata.sql
@@ -1,12 +1,26 @@
-CREATE OR REPLACE FUNCTION grest.pool_metadata(_pool_bech32 text)
-RETURNS JSON STABLE LANGUAGE PLPGSQL AS $$
+CREATE OR REPLACE FUNCTION grest.pool_metadata (_pool_bech32 text)
+    RETURNS json STABLE
+    LANGUAGE PLPGSQL
+    AS $$
 BEGIN
-    RETURN ( SELECT json_build_object(
-        'meta_url', pmd.url,
-        'meta_hash', encode(pmd.hash::bytea, 'hex')
-    )
-    FROM public.pool_meta_data AS pmd
-    WHERE pmd.id = (SELECT id from public.pool_hash where view=_pool_bech32)
-    );
-END; $$;
+    RETURN (
+        SELECT
+            json_build_object('meta_url', pmd.url, 'meta_hash', encode(pmd.hash::bytea, 'hex'))
+        FROM
+            public.pool_metadata_ref AS pmd
+        WHERE
+            pmd.pool_id = (
+                SELECT
+                    id
+                FROM
+                    public.pool_hash
+                WHERE
+                    VIEW = _pool_bech32)
+            ORDER BY
+                pmd.registered_tx_id DESC
+            LIMIT 1);
+END;
+$$;
+
 COMMENT ON FUNCTION grest.pool_metadata IS 'Get pool metadata url and hash';
+
