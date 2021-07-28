a83b820d63dd671a9e195c2edbc76230f233719f
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-07-28 20:10:43 +0100

    Add null condition

--- files/grest/rpc/pool/pool_metadata.sql
+++ files/grest/rpc/pool/pool_metadata.sql
@@ -3,24 +3,38 @@ CREATE OR REPLACE FUNCTION grest.pool_metadata (_pool_bech32 text)
     LANGUAGE PLPGSQL
     AS $$
 BEGIN
-    RETURN (
-        SELECT
-            json_build_object('meta_url', pmd.url, 'meta_hash', encode(pmd.hash::bytea, 'hex'))
-        FROM
-            public.pool_metadata_ref AS pmd
-        WHERE
-            pmd.pool_id = (
-                SELECT
-                    id
+    IF _pool_bech32 IS NULL THEN
+        RETURN (
+            SELECT
+                json_agg(js) json_final
+            FROM ( SELECT DISTINCT ON (ph.id)
+                    json_build_object('pool_id', ph.view, 'meta_url', pmr.url, 'meta_hash', encode(pmr.hash::bytea, 'hex')) js
                 FROM
-                    public.pool_hash
-                WHERE
-                    VIEW = _pool_bech32)
-            ORDER BY
-                pmd.registered_tx_id DESC
-            LIMIT 1);
+                    public.pool_metadata_ref AS pmr
+                    INNER JOIN pool_hash AS ph ON ph.id = pmr.pool_id
+                ORDER BY
+                    ph.id,
+                    pmr.registered_tx_id DESC) t);
+    ELSE
+        RETURN (
+            SELECT
+                json_build_object('meta_url', pmr.url, 'meta_hash', encode(pmr.hash::bytea, 'hex'))
+            FROM
+                public.pool_metadata_ref AS pmr
+            WHERE
+                pmr.pool_id = (
+                    SELECT
+                        id
+                    FROM
+                        public.pool_hash
+                    WHERE
+                        VIEW = _pool_bech32)
+                ORDER BY
+                    pmr.registered_tx_id DESC
+                LIMIT 1);
+    END IF;
 END;
 $$;
 
-COMMENT ON FUNCTION grest.pool_metadata IS 'Get pool metadata url and hash';
+COMMENT ON FUNCTION grest.pool_metadata IS 'Get pool metadata url and hash, all pools if pool_id empty';
 
