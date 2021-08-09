37fb79b01e69dc41795819b3932536f23f6bc297
rdlrt <3169068+rdlrt@users.noreply.github.com>
2021-08-09 09:52:44 +0000

    Change result object from JSON to Table for rewards_history and totals

--- files/grest/rpc/blockchain/rewards_history.sql
+++ files/grest/rpc/blockchain/rewards_history.sql
@@ -1,37 +1,31 @@
-CREATE OR REPLACE FUNCTION grest.rewards_history (_stake_address_bech32 text DEFAULT NULL, _epoch_no numeric DEFAULT NULL)
-    RETURNS json STABLE
+CREATE OR REPLACE FUNCTION grest.rewards_history (_stake_address text DEFAULT NULL, _epoch_no numeric DEFAULT NULL)
+    RETURNS TABLE (earned_epoch bigint, stake_pool character varying, reward_amount lovelace, reward_type rewardtype, spendable_epoch bigint)
     LANGUAGE PLPGSQL
     AS $$
 BEGIN
     IF _epoch_no IS NULL THEN
-        RETURN (
+        RETURN QUERY (
             SELECT
-                json_agg(js) json_final
-            FROM (
-                SELECT
-                    json_build_object('earned_epoch', reward.earned_epoch, 'stake_pool', pool_hash.view, 'reward_amount', reward.amount, 'reward_type', reward.type, 'spendable_epoch', reward.spendable_epoch) js
-                FROM
-                    reward
-                    INNER JOIN stake_address ON reward.addr_id = stake_address.id
-                    INNER JOIN pool_hash ON reward.pool_id = pool_hash.id
-                WHERE
-                    stake_address.view = _stake_address_bech32
-                ORDER BY
-                    spendable_epoch DESC) t);
+                r.earned_epoch, ph.view, r.amount, r.type, r.spendable_epoch
+            FROM
+                reward AS r
+                INNER JOIN stake_address AS sa ON r.addr_id = sa.id
+                INNER JOIN pool_hash AS ph ON r.pool_id = ph.id
+            WHERE
+                sa.view = _stake_address
+            ORDER BY
+                r.spendable_epoch DESC);
     ELSE
-        RETURN (
+        RETURN QUERY (
             SELECT
-                json_agg(js) json_final
-            FROM (
-                SELECT
-                    json_build_object('earned_epoch', reward.earned_epoch, 'stake_pool', pool_hash.view, 'reward_amount', reward.amount, 'reward_type', reward.type, 'spendable_epoch', reward.spendable_epoch) js
-                FROM
-                    reward
-                    INNER JOIN stake_address ON reward.addr_id = stake_address.id
-                    INNER JOIN pool_hash ON reward.pool_id = pool_hash.id
-                WHERE
-                    stake_address.view = _stake_address_bech32
-                    AND reward.earned_epoch = _epoch_no) t);
+                r.earned_epoch, ph.view, r.amount, r.type, r.spendable_epoch
+            FROM
+                reward AS r
+                INNER JOIN stake_address AS sa ON r.addr_id = sa.id
+                INNER JOIN pool_hash AS ph ON r.pool_id = ph.id
+            WHERE
+                sa.view = _stake_address
+                AND r.earned_epoch = _epoch_no);
     END IF;
 END;
 $$;
