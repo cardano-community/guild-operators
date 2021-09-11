d717d48bdbaff67f7e8f5041afbc804ae53a0345
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-09-11 21:58:47 +0100

    Add trigger for new epoch active stake cache

--- files/grest/rpc/cached_tables/active_stake_cache.sql
+++ files/grest/rpc/cached_tables/active_stake_cache.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS GREST.ACTIVE_STAKE_CACHE (
-    POOL_ID varchar, -- Index created after initial data insert
-    EPOCH_NO bigint,
-    AMOUNT lovelace
+    POOL_ID varchar NOT NULL,
+    EPOCH_NO bigint NOT NULL,
+    AMOUNT LOVELACE NOT NULL,
+    PRIMARY KEY (POOL_ID, EPOCH_NO)
 );
 
 INSERT INTO GREST.ACTIVE_STAKE_CACHE
@@ -16,6 +17,52 @@ GROUP BY
     POOL_HASH.VIEW,
     EPOCH_STAKE.EPOCH_NO;
 
-CREATE INDEX IF NOT EXISTS pool_id_idx ON GREST.ACTIVE_STAKE_CACHE (pool_id);
+-- Trigger for inserting new epoch active stake data
+DROP FUNCTION IF EXISTS GREST.ACTIVE_STAKE_EPOCH_UPDATE CASCADE;
 
--- TODO: current_epoch + 1 entries (needs to be manually calculated via delegation table)
+CREATE FUNCTION GREST.ACTIVE_STAKE_EPOCH_UPDATE ()
+    RETURNS TRIGGER
+    AS $active_stake_epoch_update$
+DECLARE
+    _current_epoch integer DEFAULT NULL;
+BEGIN
+    SELECT
+        MAX(epoch_no)
+    FROM
+        public.epoch_stake INTO _current_epoch;
+    INSERT INTO grest.ACTIVE_STAKE_CACHE
+    SELECT
+        POOL_HASH.VIEW,
+        EPOCH_STAKE.EPOCH_NO,
+        SUM(EPOCH_STAKE.AMOUNT)
+    FROM
+        EPOCH_STAKE
+        INNER JOIN POOL_HASH ON POOL_HASH.ID = EPOCH_STAKE.POOL_ID
+    WHERE
+        epoch_stake.epoch_no = _current_epoch
+    GROUP BY
+        POOL_HASH.VIEW,
+        EPOCH_STAKE.EPOCH_NO
+        -- At the moment, epoch_stake gets inserts in chunks of 2000 and there is
+        -- no way to figure out whether a given chunk is the last one. This means
+        -- that we have to run the function on every chunk so the conflict clause
+        -- below is skipping inserts that we already processed.
+        -- This conflict clause should be removed once db-syncs has an event for
+        -- epoch_stake inserts completion. Then, the trigger can be switched
+        -- to be based on that event and ran only once for each new epoch.
+        -- Linked issue: https://github.com/input-output-hk/cardano-db-sync/issues/797
+    ON CONFLICT (POOL_ID,
+        EPOCH_NO)
+        DO NOTHING;
+    RETURN NULL;
+END;
+$active_stake_epoch_update$
+LANGUAGE PLPGSQL;
+
+DROP TRIGGER IF EXISTS ACTIVE_STAKE_EPOCH_UPDATE_TRIGGER ON PUBLIC.EPOCH_STAKE;
+
+CREATE TRIGGER ACTIVE_STAKE_EPOCH_UPDATE_TRIGGER
+    AFTER INSERT ON PUBLIC.EPOCH_STAKE
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION GREST.ACTIVE_STAKE_EPOCH_UPDATE ();
+
