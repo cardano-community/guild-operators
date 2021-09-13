be7bb639fad9c6a480dd423ff23f6f69c78329c8
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-09-13 18:26:41 +0100

    Styling changes on blocks rpc

--- files/grest/rpc/blocks/blocks.sql
+++ files/grest/rpc/blocks/blocks.sql
@@ -2,39 +2,33 @@ DROP FUNCTION IF EXISTS grest.blocks ();
 
 CREATE FUNCTION grest.blocks ()
   RETURNS TABLE (
-    hash text,
-    epoch uinteger,
-    abs_slot uinteger,
-    epoch_slot uinteger,
-    block_no uinteger,
-    block_time timestamp,
-    tx_count bigint,
-    vrf_key varchar,
-    op_cert_counter word63type,
-    pool varchar,
-    parent_hash text)
+    HASH text,
+    EPOCH uinteger,
+    ABS_SLOT uinteger,
+    EPOCH_SLOT uinteger,
+    HEIGHT uinteger,
+    TIME timestamp,
+    TX_COUNT bigint,
+    VRF_KEY varchar,
+    OP_CERT_COUNTER word63type,
+    POOL varchar,
+    PARENT_HASH text)
   LANGUAGE PLPGSQL
   AS $$
 BEGIN
   RETURN QUERY
   SELECT
-    ENCODE(B.HASH::bytea, 'hex') AS BLOCK_HASH,
+    ENCODE(B.HASH::bytea, 'hex') AS HASH,
     b.EPOCH_NO AS EPOCH,
     b.SLOT_NO AS ABS_SLOT,
     b.EPOCH_SLOT_NO AS EPOCH_SLOT,
-    b.BLOCK_NO,
+    b.BLOCK_NO AS HEIGHT,
     b.TIME,
     b.TX_COUNT,
     b.VRF_KEY,
     b.OP_CERT_COUNTER,
-    ph.VIEW,
-    (
-      SELECT
-        ENCODE(tB.HASH::bytea, 'hex')
-      FROM
-        block tB
-      WHERE
-        id = b.id - 1) AS parent_hash
+    ph.VIEW AS POOL,
+    LAG(ENCODE(b.HASH::bytea, 'hex')) OVER (ORDER BY b.ID) AS PARENT_HASH
   FROM
     BLOCK B
   LEFT JOIN SLOT_LEADER SL ON SL.ID = B.SLOT_LEADER_ID
@@ -45,3 +39,4 @@ END;
 $$;
 
 COMMENT ON FUNCTION grest.blocks IS 'Get detailed information about all blocks (paginated - latest first)';
+
