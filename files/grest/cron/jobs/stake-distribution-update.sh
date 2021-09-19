34e379f7bb605c29850739bba2e81103a0b34797
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-09-19 09:37:17 +0100

    Better logging

--- files/grest/cron/jobs/stake-distribution-update.sh
+++ files/grest/cron/jobs/stake-distribution-update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LOG_DIR="${CNODE_HOME}/logs"
-psql cexplorer -c "SELECT GREST.UPDATE_STAKE_DISTRIBUTION_CACHE_CHECK();" >>"${LOG_DIR}/stake-distribution-update.log"
+echo "$(date +%F_%H:%M:%S) Running stake distribution update..."
+psql cexplorer -qbt -c "SELECT GREST.UPDATE_STAKE_DISTRIBUTION_CACHE_CHECK();"
