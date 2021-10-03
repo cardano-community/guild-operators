276c79d21832da6f966c70a1897b2ba362a1905f
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-10-03 17:31:41 +0100

    Switch UTXO set to address_info RPC

--- files/grest/rpc/address/address_info.sql
+++ files/grest/rpc/address/address_info.sql
@@ -3,7 +3,8 @@ DROP FUNCTION IF EXISTS grest.address_info (text);
 CREATE FUNCTION grest.address_info (_payment_address text DEFAULT NULL)
   RETURNS TABLE (
     balance numeric,
-    stake_address text)
+    stake_address text,
+    utxo_set json)
   LANGUAGE PLPGSQL
   AS $$
 DECLARE
@@ -17,7 +18,6 @@ BEGIN
   WHERE
     address = _payment_address
   LIMIT 1 INTO _stake_address_id;
-  RAISE NOTICE 'sa_id (%)', _stake_address_id;
   IF _stake_address_id IS NOT NULL THEN
     SELECT
       view
@@ -29,16 +29,18 @@ BEGIN
   RETURN QUERY
   SELECT
     SUM(tx_out.value),
-    _stake_address
+    _stake_address,
+    JSON_AGG(JSON_BUILD_OBJECT('tx_hash', ENCODE(tx.hash, 'hex'), 'tx_index', tx_out.index, 'value', tx_out.value)) utxo_set
   FROM
     public.tx_out
-  LEFT JOIN public.tx_in ON tx_in.tx_out_id = tx_out.tx_id
-    AND tx_in.tx_out_index = tx_out.index
-WHERE
-  tx_in.id IS NULL
+    INNER JOIN public.tx ON tx_out.tx_id = tx.id
+    LEFT JOIN public.tx_in ON tx_in.tx_out_id = tx_out.tx_id
+      AND tx_in.tx_out_index = tx_out.index
+  WHERE
+    tx_in.id IS NULL
     AND tx_out.address = _payment_address;
 END;
 $$;
 
-COMMENT ON FUNCTION grest.address_info IS 'Get payment address info - balance and associated stake address (if any)';
+COMMENT ON FUNCTION grest.address_info IS 'Get payment address info - balance, associated stake address (if any) and UTXO set';
 
