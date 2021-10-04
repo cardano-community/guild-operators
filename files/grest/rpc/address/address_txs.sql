df60b940cd9e16fff9d250ac236c03b96e3c9e8e
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-10-04 20:02:13 +0100

    initial address_txs rpc

---
+++ files/grest/rpc/address/address_txs.sql
@@ -0,0 +1,34 @@
+DROP FUNCTION IF EXISTS grest.address_txs (text);
+
+CREATE FUNCTION grest.address_txs (_payment_address text, _after_block_height integer DEFAULT NULL)
+  RETURNS TABLE (
+    tx_hash text)
+  LANGUAGE PLPGSQL
+  AS $$
+BEGIN
+  IF _after_block_height IS NOT NULL THEN
+    RETURN QUERY
+    SELECT
+      ENCODE(tx.hash, 'hex') as tx_hash
+    FROM
+      public.tx_out
+      INNER JOIN public.tx ON tx_out.tx_id = tx.id
+      INNER JOIN public.block ON block.id = tx.block_id
+    WHERE
+      tx_out.address = _payment_address
+      AND block.block_no >= _after_block_height;
+  ELSE
+    RETURN QUERY
+    SELECT
+      ENCODE(tx.hash, 'hex') as tx_hash
+    FROM
+      public.tx_out
+      INNER JOIN public.tx ON tx_out.tx_id = tx.id
+    WHERE
+      tx_out.address = _payment_address;
+  END IF;
+END;
+$$;
+
+COMMENT ON FUNCTION grest.address_txs IS 'Get transaction hash list of a payment address, optionally filtering after specified block height.';
+
