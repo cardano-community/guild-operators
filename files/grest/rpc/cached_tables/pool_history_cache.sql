06af930ca357aff67d64217034d3112000aa46b2
KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>
2021-11-02 22:35:15 +1100

    Couple of tweaks
    Added concurrent execution check as per Damjen's advice, used LEAST in ROS calculation to prevent chances of numeric overflow due to dodgy data (e.g. thousands of Ada rewards for less than 1 ADA active stake in Priyank's database).

--- files/grest/rpc/cached_tables/pool_history_cache.sql
+++ files/grest/rpc/cached_tables/pool_history_cache.sql
@@ -30,6 +30,16 @@ declare
   _curr_epoch bigint;
   _latest_epoch_no_in_cache bigint;
 begin
+   IF (
+    SELECT
+      COUNT(pid) > 1
+    FROM
+      pg_stat_activity
+    WHERE
+      query != '<IDLE>' AND query ILIKE 'SELECT GREST.pool_history_cache_update%') THEN
+    RAISE EXCEPTION 'Previous pool_history_cache_update query still running but should have completed! Exiting...';
+  END IF;
+
   if _epoch_no_to_insert_from is null then
     select
       COALESCE(MAX(epoch_no), 0) into _latest_epoch_no_in_cache
@@ -215,7 +225,9 @@ begin
                             when 0 then
                               null
                             else
-                              ROUND((((POW(((((COALESCE(m.memtotal, 0) + (COALESCE(l.leadertotal, 0) - (actf.pool_fee_fixed + (((COALESCE(m.memtotal, 0) + COALESCE(l.leadertotal, 0)) - actf.pool_fee_fixed) * actf.pool_fee_variable))))) / (NULLIF(actf.active_stake,0))) + 1), 73) - 1)) * 100)::numeric, 2)
+                              -- using LEAST as a way to prevent overflow, in case of dodgy database data (e.g. giant rewards / tiny active stake)
+                              ROUND((((POW(( LEAST(( ((COALESCE(m.memtotal, 0) + (COALESCE(l.leadertotal, 0) - (actf.pool_fee_fixed + (((COALESCE(m.memtotal, 0) + 
+								              COALESCE(l.leadertotal, 0)) - actf.pool_fee_fixed) * actf.pool_fee_variable))))) / (NULLIF(actf.active_stake,0)) ), 1000) + 1), 73) - 1)) * 100)::numeric, 2)
                             end
                           end epoch_ros
                         from
@@ -243,8 +255,5 @@ current-epoch-minus-one. Invoke with non-empty param for initial population, wit
 
 -- initial population of the history table, will take longer as the number of Cardano epochs grows
 -- if we decide to remove the below and let cron-based invocation to populate it then need to adjust the update function logic and remove special case for empty table handling
-select
-  *
-from
-  grest.pool_history_cache_update (0);
+select * from grest.pool_history_cache_update (0);
 
