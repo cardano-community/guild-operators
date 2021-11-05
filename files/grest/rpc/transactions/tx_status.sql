751a25ba449768eb3d30519a179ad5250ca721d6
KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>
2021-11-05 11:39:02 +1100

    Simplified the query

--- files/grest/rpc/transactions/tx_status.sql
+++ files/grest/rpc/transactions/tx_status.sql
@@ -15,24 +15,12 @@ BEGIN
         block b;
 
     RETURN QUERY (
-		WITH matched_txs AS (
-            SELECT
-                t.hash, b.block_no FROM block b, tx t
-            WHERE
-                t.hash::bytea = ANY (
-                    SELECT
-                        DECODE(HASHES, 'hex')
-                FROM UNNEST(_tx_hashes) AS hashes)
-        AND t.block_id = b.id)
-
-	-- returns difference in block numbers, or null if tx was not found
-	SELECT 
-		HASHES, (_curr_block_no - m.block_no)
-    FROM 
-		UNNEST(_tx_hashes) WITH ORDINALITY HASHES
-    LEFT OUTER JOIN 
-		matched_txs m ON m.hash = DECODE(HASHES, 'hex')
-	ORDER BY ORDINALITY);
+    	select HASHES, (_curr_block_no - b.block_no)
+    	from UNNEST(_tx_hashes) WITH ORDINALITY HASHES
+    	left outer join tx t on t.hash = DECODE(HASHES, 'hex')
+    	left outer join block b on t.block_id = b.id
+    	ORDER BY ordinality
+    	);
 END;
 $$;
 
