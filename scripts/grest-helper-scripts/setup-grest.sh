63041ce55827bd6090fb404b3ce3ccb6b485cf36
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-11-09 20:59:27 +0000

    Update echo

--- scripts/grest-helper-scripts/setup-grest.sh
+++ scripts/grest-helper-scripts/setup-grest.sh
@@ -123,7 +123,7 @@
 
   # Description : Remove all grest-related cron entries.
   remove_all_cron_jobs() {
-    echo "Remove all installed cron jobs..."
+    echo "Removing all installed cron jobs..."
     remove_cron_job "stake-distribution-update"
     remove_cron_job "pool-history-cache-update"
   }
