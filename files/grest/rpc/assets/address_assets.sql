e6e0f20c2e226eba8f1b084c21e08bb902ac7092
KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>
2021-11-11 23:43:54 +1100

    Very simple address assets endpoint

---
+++ files/grest/rpc/assets/address_assets.sql
@@ -0,0 +1,31 @@
+DROP FUNCTION IF EXISTS grest.address_assets (_address text);
+
+
+CREATE FUNCTION grest.address_assets (_address text)
+	RETURNS TABLE (
+		asset_policy_hex text, 
+		asset_name_hex text, 
+		quantity word64type) 
+	LANGUAGE PLPGSQL 
+	AS $$
+
+BEGIN
+	return QUERY
+	select
+		encode(policy, 'hex') as hex_policy,
+		encode(name, 'escape') escaped_name,
+		mtx.quantity
+	from
+		MA_TX_OUT MTX
+	inner join TX_OUT TXO on
+		TXO.ID = MTX.TX_OUT_ID
+	left join TX_IN on
+		TXO.TX_ID = TX_IN.TX_OUT_ID
+		and TXO.INDEX::smallint = TX_IN.TX_OUT_INDEX::smallint
+	where
+		txo.address = _address
+		and TX_IN.TX_IN_ID is null;
+END;
+$$;
+
+COMMENT ON FUNCTION grest.address_assets IS 'Get the list of all the assets (policy, name and quantity) for a given address';
