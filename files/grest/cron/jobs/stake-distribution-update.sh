7cb1817f9ea73b20f1e2e2c4feaf51f58b015161
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-11-14 13:43:53 +0000

    Fix stake distribution cron script

--- files/grest/cron/jobs/stake-distribution-update.sh
+++ files/grest/cron/jobs/stake-distribution-update.sh
@@ -2,4 +2,4 @@
 DB_NAME=cexplorer
 
 echo "$(date +%F_%H:%M:%S) Running stake distribution update..."
-psql ${DB_NAME} -qbt -c "SELECT GREST.UPDATE_STAKE_DISTRIBUTION_CACHE_CHECK();" 2>&1 1>/dev/null
+psql ${DB_NAME} -qbt -c "SELECT GREST.STAKE_DISTRIBUTION_CACHE_UPDATE_CHECK();" 2>&1 1>/dev/null
