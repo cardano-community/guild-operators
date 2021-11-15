b85ce71794cd11d7dafd023ba799e7291abad465
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-11-15 05:51:42 +0000

    Add param to reset the grest node (#1186)
    * [WIP] Reset grest schema param
    
    * Check update bug fix for setup-grest.sh
    
    * DB_SCRIPTS_URL env var
    
    * clean up
    
    * Add support for dropping triggers
    
    * checkUpdate fix
    
    * Add echo to update check
    
    * Fix reset SQL
    
    * Remove cron jobs
    
    * Add echo
    
    * reset_grest function
    
    * Update echo
    
    * Use quiet psql
    
    * Move control table setup to basics
    
    * Move basics setup to deploy_query_updates()
    
    * Exit after updating script
    
    * Fix checkupdate ifs
    
    * Remove cron jobs before resetting
    
    * update usage
    
    * Fix check_db_status call
    
    * update check, cron script changes and WIP for token registry
    
    * fix update re-launch and sed on cron scripts
    
    * asset_registry_cache_update function and asset_info update incl updates to token registry code
    
    * fixes
    
    * setup output alignment
    
    * fixes
    
    * fix asset_info comparision
    
    * Add default backup (koios) servers to haproxy configs - need to register testnet DNS
    
    * Move genesis table to basics.sql
    
    * rename asset registry cache
    
    * Review changes and docs
    
    * clean up
    
    * kill running cron jobs before full reset
    
    * sudo
    
    * Use dollar quoted strings instead of single quote
    
    * Kill running cron jobs
    
    * Fix find query pid function
    
    * db_basics needed for reset
    
    * Add return code to db_basics_setup
    
    * Kill cron jobs gracefully
    
    * Fix stake distribution cron script
    
    * Gracefully handle no PID calls
    
    * Fix psql kill command
    
    * Fix re-dir for pkill
    
    Co-authored-by: Ola <ola.ahlman@gmail.com>
    Co-authored-by: rdlrt <3169068+rdlrt@users.noreply.github.com>

--- files/grest/rpc/cached_tables/stake_distribution_cache.sql
+++ files/grest/rpc/cached_tables/stake_distribution_cache.sql
@@ -10,12 +10,6 @@ CREATE TABLE IF NOT EXISTS GREST.STAKE_DISTRIBUTION_CACHE (
   TREASURY numeric
 );
 
-CREATE TABLE IF NOT EXISTS GREST.CONTROL_TABLE (
-  key text PRIMARY KEY,
-  last_value text NOT NULL,
-  artifacts text
-);
-
 DROP PROCEDURE IF EXISTS GREST.UPDATE_STAKE_DISTRIBUTION_CACHE ();
 
 DROP FUNCTION IF EXISTS GREST.UPDATE_STAKE_DISTRIBUTION_CACHE ();
@@ -181,9 +175,9 @@ ON CONFLICT (STAKE_ADDRESS)
 END;
 $$;
 
-DROP FUNCTION IF EXISTS GREST.UPDATE_STAKE_DISTRIBUTION_CACHE_CHECK;
+DROP FUNCTION IF EXISTS GREST.STAKE_DISTRIBUTION_CACHE_UPDATE_CHECK;
 
-CREATE FUNCTION GREST.UPDATE_STAKE_DISTRIBUTION_CACHE_CHECK ()
+CREATE FUNCTION GREST.STAKE_DISTRIBUTION_CACHE_UPDATE_CHECK ()
   RETURNS VOID
   LANGUAGE PLPGSQL
   AS $$
@@ -202,18 +196,16 @@ BEGIN
     FROM
       pg_stat_activity
     WHERE
-      state = 'active' AND query ILIKE '%GREST.UPDATE_STAKE_DISTRIBUTION_CACHE_CHECK(%') THEN
+      state = 'active' AND query ILIKE '%GREST.STAKE_DISTRIBUTION_CACHE_UPDATE_CHECK(%') THEN
     RAISE EXCEPTION 'Previous query still running but should have completed! Exiting...';
   END IF;
   -- QUERY START --
-  SELECT COALESCE(
-    (SELECT
-      last_value::bigint
-    FROM
-      GREST.control_table
-    WHERE
-      key = 'stake_distribution_lbh')
-  , 0) INTO _last_update_block_height;
+  SELECT
+    COALESCE((
+      SELECT
+        last_value::bigint FROM GREST.control_table
+      WHERE
+        key = 'stake_distribution_lbh'), 0) INTO _last_update_block_height;
   SELECT
     MAX(block_no)
   FROM
---
+++ scripts/grest-helper-scripts/db-scripts/basics.sql
@@ -0,0 +1,114 @@
+--------------------------------------------------------------------------------
+-- Entry point for Koios node DB setup:
+-- 1) grest schema that will hold all RPC functions/views and cached tables
+-- 2) web_anon user
+-- 3) grest.control_table
+-- 4) grest.genesis
+-- 5) helper functions
+-- 6) optional db indexes on important public tables
+--------------------------------------------------------------------------------
+-- GREST SCHEMA --
+CREATE SCHEMA IF NOT EXISTS grest;
+
+-- WEB_ANON USER --
+DO $$
+BEGIN
+  CREATE ROLE web_anon nologin;
+EXCEPTION
+  WHEN DUPLICATE_OBJECT THEN
+    RAISE NOTICE 'web_anon exists, skipping...';
+END
+$$;
+
+GRANT USAGE ON SCHEMA public TO web_anon;
+
+GRANT USAGE ON SCHEMA grest TO web_anon;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO web_anon;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA grest TO web_anon;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT
+SELECT
+  ON TABLES TO web_anon;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA grest GRANT
+SELECT
+  ON TABLES TO web_anon;
+
+ALTER ROLE web_anon SET search_path TO grest, public;
+
+-- CONTROL TABLE --
+CREATE TABLE IF NOT EXISTS GREST.CONTROL_TABLE (
+  key text PRIMARY KEY,
+  last_value text NOT NULL,
+  artifacts text
+);
+
+-- GENESIS TABLE --
+DROP TABLE IF EXISTS grest.genesis;
+
+-- Data Types are intentionally kept varchar for single ID row to avoid future edge cases
+CREATE TABLE grest.genesis (
+  NETWORKMAGIC varchar,
+  NETWORKID varchar,
+  ACTIVESLOTCOEFF varchar,
+  UPDATEQUORUM varchar,
+  MAXLOVELACESUPPLY varchar,
+  EPOCHLENGTH varchar,
+  SYSTEMSTART varchar,
+  SLOTSPERKESPERIOD varchar,
+  SLOTLENGTH varchar,
+  MAXKESREVOLUTIONS varchar,
+  SECURITYPARAM varchar,
+  ALONZOGENESIS varchar
+);
+
+-- HELPER FUNCTIONS --
+DROP FUNCTION IF EXISTS grest.get_query_pids_partial_match (_query text);
+
+CREATE FUNCTION grest.get_query_pids_partial_match (_query text)
+  RETURNS TABLE (
+    pid integer)
+  LANGUAGE plpgsql
+  AS $$
+BEGIN
+  RETURN QUERY
+  SELECT
+    pg_stat_activity.pid
+  FROM
+    pg_stat_activity
+  WHERE
+    query ILIKE '%' || _query || '%'
+    AND query NOT ILIKE '%grest.get_query_pids_partial_match%';
+END;
+$$;
+
+-- Not really fully tested nor working yet, but in case we'll need something like this for the future.
+/* DROP PROCEDURE IF EXISTS grest.kill_queries_partial_match (_query text);
+
+CREATE PROCEDURE grest.kill_queries_partial_match (_query text)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+ _pids integer[];
+BEGIN
+ _pids := ARRAY (
+ SELECT
+ grest.get_query_pids_partial_match (_query));
+ FOR i IN 1..ARRAY_UPPER(_pids, 1)
+ LOOP
+ RAISE NOTICE 'Cancelling PID: %', _pids[i];
+ PERFORM
+ PG_CANCEL_BACKEND(_pids[i]);
+ END LOOP;
+END;
+$$; */
+--
+--
+-- DATABASE INDEXES --
+-- Most likely deprecated after 12.0.0
+CREATE INDEX IF NOT EXISTS _asset_policy_idx ON PUBLIC.MA_TX_OUT ( policy);
+
+CREATE INDEX IF NOT EXISTS _asset_identifier_idx ON PUBLIC.MA_TX_OUT ( policy, name);
+
