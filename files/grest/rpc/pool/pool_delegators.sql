4d3c2a50f9fe4d2b18a8cd4fc7f871b508801bb3
Ola <ola.ahlman@gmail.com>
2021-12-06 17:21:50 +0100

    Updated pool_delegators endpoint to take optional epoch

--- files/grest/rpc/pool/pool_delegators.sql
+++ files/grest/rpc/pool/pool_delegators.sql
@@ -1,25 +1,48 @@
-DROP FUNCTION IF EXISTS grest.pool_delegators (text);
+DROP FUNCTION IF EXISTS grest.pool_delegators (text, uinteger);
 
-CREATE FUNCTION grest.pool_delegators (_pool_bech32 text)
-    RETURNS TABLE (
-        stake_address character varying,
-        live_stake numeric
-    )
-    LANGUAGE plpgsql
-    AS $$
-    #variable_conflict use_column
+CREATE FUNCTION grest.pool_delegators (_pool_bech32 text, _epoch_no uinteger DEFAULT NULL)
+  RETURNS TABLE (
+    stake_address character varying,
+    amount lovelace,
+    epoch_no uinteger
+  )
+  LANGUAGE plpgsql
+  AS $$
+  #variable_conflict use_column
+DECLARE
+  _pool_id bigint;
 BEGIN
+  SELECT id INTO _pool_id FROM pool_hash WHERE pool_hash.view = _pool_bech32;
+
+  IF _epoch_no IS NULL THEN
     RETURN QUERY
-    SELECT
+      SELECT
         stake_address,
-        total_balance
-    FROM
+        total_balance::lovelace,
+        (SELECT MAX(no) FROM public.epoch)::uinteger
+      FROM
         grest.stake_distribution_cache AS sdc
-    WHERE
+      WHERE
         sdc.pool_id = _pool_bech32
-    ORDER BY
+      ORDER BY
         sdc.total_balance DESC;
+  ELSE
+    RETURN QUERY
+      SELECT
+        SA.view,
+        ES.amount,
+        _epoch_no
+      FROM
+        public.epoch_stake ES
+        INNER JOIN public.stake_address SA ON ES.addr_id = SA.id
+      WHERE
+        ES.pool_id = _pool_id
+        AND
+        ES.epoch_no = _epoch_no
+      ORDER BY
+        ES.amount DESC;
+  END IF;
 END;
 $$;
 
-COMMENT ON FUNCTION grest.pool_delegators IS 'Return information about current delegators by a given pool';
+COMMENT ON FUNCTION grest.pool_delegators IS 'Return information about delegators by a given pool and epoch number, current if no epoch provided.';
