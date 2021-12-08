9a4cfe1fc52b02e05e95d23e0a3bddc60ac6c2c2
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-12-08 17:20:00 +0000

    Fix sql comments

--- files/docker/grest/docker-entrypoint-initdb/01_postgres_config.sql
+++ files/docker/grest/docker-entrypoint-initdb/01_postgres_config.sql
@@ -1,9 +1,9 @@
 ALTER SYSTEM
 SET checkpoint_timeout = '5min';
-#ALTER SYSTEM
-#SET max_segment_size = '1GB';
-#ALTER SYSTEM
-#SET max_wal_size = '2GB';
+--ALTER SYSTEM
+--SET max_segment_size = '1GB';
+--ALTER SYSTEM
+--SET max_wal_size = '2GB';
 ALTER SYSTEM
 SET min_wal_size = '300MB';
 ALTER SYSTEM
