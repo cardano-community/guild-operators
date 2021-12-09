63f920cb6a573f0e1ad578d876f7dfe59cc289c1
Ola [AHLNET] <ola.ahlman@gmail.com>
2021-12-09 13:02:20 +0100

    api update, asset_address_list alignment & checkstatus fix (#1218)

--- files/grest/rpc/assets/asset_address_list.sql
+++ files/grest/rpc/assets/asset_address_list.sql
@@ -6,14 +6,7 @@ CREATE FUNCTION grest.asset_address_list (_asset_policy text, _asset_name text)
     quantity numeric)
   LANGUAGE PLPGSQL
   AS $$
-DECLARE
-  _asset_policy_decoded bytea;
-  _asset_name_decoded bytea;
 BEGIN
-  SELECT
-    DECODE(_asset_policy, 'hex') INTO _asset_policy_decoded;
-  SELECT
-    DECODE(_asset_name::text, 'escape') INTO _asset_name_decoded;
   RETURN QUERY
   SELECT
     TXO.ADDRESS,
@@ -25,8 +18,8 @@ BEGIN
     LEFT JOIN TX_IN ON TXO.TX_ID = TX_IN.TX_OUT_ID
       AND TXO.INDEX::smallint = TX_IN.TX_OUT_INDEX::smallint
   WHERE
-    MA.policy = _asset_policy_decoded
-    AND MA.name = _asset_name_decoded
+    MA.policy = _asset_policy
+    AND MA.name = _asset_name
     AND TX_IN.TX_IN_ID IS NULL
   GROUP BY
     TXO.ADDRESS;
