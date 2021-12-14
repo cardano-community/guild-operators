84f744d865b8a8f4fd151d23c3add6904f974fd8
illuminatus <trevor.benson@gmail.com>
2021-12-13 17:30:51 -0800

    Move ncurses-libs back to main list.

--- scripts/cnode-helper-scripts/prereqs.sh
+++ scripts/cnode-helper-scripts/prereqs.sh
@@ -194,13 +194,13 @@ if [ "$WANT_BUILD_DEPS" = 'Y' ]; then
     $sudo yum -y install curl > /dev/null
     $sudo yum -y update > /dev/null
     echo "  Installing missing prerequisite packages, if any.."
-    pkg_list="python3 coreutils libffi-devel gmp-devel openssl-devel systemd systemd-devel libsodium-devel zlib-devel make gcc-c++ tmux git jq gnupg2 libtool autoconf iproute bc traceroute dialog sqlite util-linux xz"
+    pkg_list="python3 coreutils libffi-devel gmp-devel openssl-devel ncurses-libs systemd systemd-devel libsodium-devel zlib-devel make gcc-c++ tmux git jq gnupg2 libtool autoconf iproute bc traceroute dialog sqlite util-linux xz"
     if [[ "${VERSION_ID}" == "7" ]]; then
-      pkg_list="${pkg_list} libusb ncurses-libs pkconfig srm"
+      pkg_list="${pkg_list} libusb pkconfig srm"
     elif [[ "${VERSION_ID}" == "8" ]]; then
-      pkg_list="${pkg_list} libusbx ncurses-libs ncurses-compat-libs pkconf-pkg-config"
+      pkg_list="${pkg_list} libusbx ncurses-compat-libs pkconf-pkg-config"
     elif [[ "${DISTRO}" =~ Fedora ]]; then
-      pkg_list="${pkg_list} libusbx ncurses-libs ncurses-compat-libs pkconf-pkg-config srm"
+      pkg_list="${pkg_list} libusbx ncurses-compat-libs pkconf-pkg-config srm"
     fi
     ! grep -q ^epel <<< "$(yum repolist)" && sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-"$(grep ^VERSION_ID /etc/os-release | cut -d\" -f2)".noarch.rpm > /dev/null
     $sudo yum -y --allowerasing install ${pkg_list} > /dev/null;rc=$?
