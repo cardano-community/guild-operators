f875178d604e07c114d3dd7142565f618e134d87
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2021-12-16 08:57:37 +0000

    Add script endpoint docs

--- files/grest/rpc/script/script_list.sql
+++ files/grest/rpc/script/script_list.sql
@@ -1,15 +1,16 @@
 DROP FUNCTION IF EXISTS grest.script_list ();
 CREATE FUNCTION grest.script_list ()
 RETURNS TABLE (
-    tx_hash text,
-    script_hash text
+    script_hash text,
+    creation_tx_hash text
 )
 LANGUAGE PLPGSQL AS
 $$
 BEGIN 
 RETURN QUERY
-SELECT ENCODE(tx.hash, 'hex') as tx_hash,
-    ENCODE(script.hash, 'hex') as script_hash
+SELECT 
+    ENCODE(script.hash, 'hex') as script_hash, 
+    ENCODE(tx.hash, 'hex') as creation_tx_hash
 FROM script
     INNER JOIN tx ON tx.id = script.tx_id
 WHERE script.type = 'plutus';
