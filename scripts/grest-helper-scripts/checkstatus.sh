cc2558f1a6a54d6037a66e420d3b219fef85d39c
Priyank <3169068+rdlrt@users.noreply.github.com>
2021-12-18 18:13:54 +1100

    - Unify RPC vs root endpoints at proxy layer using ACL file (see comments in grest-poll.sh TODOs for items) (#1236)
    - Update HAProxy configs to use grestrpcs and systemd definition to use variables
    - Replace port requirement for stats with socket file, as it will always be local to HAProxy instance
    - Update checkstatus.sh according to stats update

--- scripts/grest-helper-scripts/checkstatus.sh
+++ scripts/grest-helper-scripts/checkstatus.sh
@@ -5,12 +5,10 @@
 # Common variables set in env file   #
 ######################################
 
-# HAPROXY_MON_PORT=8055       # Port where HAProxy stats can be connected to, corresponds to "stats socket" line in haproxy.cfg file
+# placeholder
 
 ######################################
 # Do NOT modify code below           #
 ######################################
 
-[[ -z ${HAPROXY_MON_PORT} ]] && HAPROXY_MON_PORT=8055
-
-watch "echo 'show stat' | nc 127.0.0.1 ${HAPROXY_MON_PORT} | grep -e svname -e ^grest | cut -d, -f 2,18,20,74 | tr ',' ' ' | column -t"
+watch 'echo "show stat" | nc -U '"$(dirname "$0")"'/../sockets/haproxy.socket | grep -e svname -e ^grest | cut -d, -f 2,18,20,74 | tr "," " " | column -t'
