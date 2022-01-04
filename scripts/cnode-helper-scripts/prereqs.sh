e00daef0d9013a04d82237abce0d4c48fdb9dff2
illuminatus <trevor.benson@gmail.com>
2022-01-03 23:46:27 -0800

    Rename pkg_options pkg_opts and add pkg_opts to apt-get for debian.

--- scripts/cnode-helper-scripts/prereqs.sh
+++ scripts/cnode-helper-scripts/prereqs.sh
@@ -175,42 +175,43 @@ if [ "$WANT_BUILD_DEPS" = 'Y' ]; then
 
   if [[ "${OS_ID}" =~ ebian ]] || [[ "${DISTRO}" =~ ebian ]]; then
     #Debian/Ubuntu
+    pkg_opts="-y"
     echo "Using apt to prepare packages for ${DISTRO} system"
     echo "  Updating system packages..."
-    $sudo apt-get -y install curl > /dev/null
-    $sudo apt-get -y update > /dev/null
+    $sudo apt-get ${pkg_opts} install curl > /dev/null
+    $sudo apt-get ${pkg_opts} update > /dev/null
     echo "  Installing missing prerequisite packages, if any.."
     pkg_list="libpq-dev python3 build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev systemd libsystemd-dev libsodium-dev zlib1g-dev make g++ tmux git jq libncursesw5 gnupg aptitude libtool autoconf secure-delete iproute2 bc tcptraceroute dialog automake sqlite3 bsdmainutils libusb-1.0-0-dev libudev-dev"
-    $sudo apt-get -y install ${pkg_list} > /dev/null;rc=$?
+    $sudo apt-get ${pkg_opts} install ${pkg_list} > /dev/null;rc=$?
     if [ $rc != 0 ]; then
       echo "An error occurred while installing the prerequisite packages, please investigate by using the command below:"
-      echo "sudo apt-get -y install ${pkg_list}"
+      echo "sudo apt-get ${pkg_opts} install ${pkg_list}"
       echo "It would be best if you could submit an issue at ${REPO} with the details to tackle in future, as some errors may be due to external/already present dependencies"
       err_exit
     fi
   elif [[ "${OS_ID}" =~ rhel ]] || [[ "${OS_ID}" =~ fedora ]] || [[ "${DISTRO}" =~ Fedora ]]; then
     #CentOS/RHEL/Fedora
-    pkg_options="-y"
+    pkg_opts="-y"
     echo "Using yum to prepare packages for ${DISTRO} system"
     echo "  Updating system packages..."
-    $sudo yum ${pkg_options} install curl > /dev/null
-    $sudo yum ${pkg_options} update > /dev/null
+    $sudo yum ${pkg_opts} install curl > /dev/null
+    $sudo yum ${pkg_opts} update > /dev/null
     echo "  Installing missing prerequisite packages, if any.."
     pkg_list="python3 coreutils libffi-devel gmp-devel openssl-devel ncurses-libs systemd systemd-devel libsodium-devel zlib-devel make gcc-c++ tmux git jq gnupg2 libtool autoconf iproute bc traceroute dialog sqlite util-linux xz"
     if [[ "${VERSION_ID}" == "2" ]] || [[ "${VERSION_ID}" == "7" ]]; then
       pkg_list="${pkg_list} libusb pkgconfig srm"
     elif [[ "${VERSION_ID}" == "8" ]]; then
-      pkg_options="${pkg_options} --allowerasing"
+      pkg_opts="${pkg_opts} --allowerasing"
       pkg_list="${pkg_list} libusbx ncurses-compat-libs pkgconf-pkg-config"
     elif [[ "${DISTRO}" =~ Fedora ]]; then
-      pkg_options="${pkg_options} --allowerasing"
+      pkg_opts="${pkg_opts} --allowerasing"
       pkg_list="${pkg_list} libusbx ncurses-compat-libs pkgconf-pkg-config srm"
     fi
-    ! grep -q ^epel <<< "$(yum repolist)" && $sudo yum ${pkg_options} install https://dl.fedoraproject.org/pub/epel/epel-release-latest-"$(grep ^VERSION_ID /etc/os-release | cut -d\" -f2)".noarch.rpm > /dev/null
-    $sudo yum ${pkg_options} install ${pkg_list} > /dev/null;rc=$?
+    ! grep -q ^epel <<< "$(yum repolist)" && $sudo yum ${pkg_opts} install https://dl.fedoraproject.org/pub/epel/epel-release-latest-"$(grep ^VERSION_ID /etc/os-release | cut -d\" -f2)".noarch.rpm > /dev/null
+    $sudo yum ${pkg_opts} install ${pkg_list} > /dev/null;rc=$?
     if [ $rc != 0 ]; then
       echo "An error occurred while installing the prerequisite packages, please investigate by using the command below:"
-      echo "sudo yum ${pkg_options} install ${pkg_list}"
+      echo "sudo yum ${pkg_opts} install ${pkg_list}"
       echo "It would be best if you could submit an issue at ${REPO} with the details to tackle in future, as some errors may be due to external/already present dependencies"
       err_exit
     fi
