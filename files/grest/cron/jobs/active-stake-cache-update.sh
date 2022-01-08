bc72dc2fc1e98ba3b48ca275e3b383cace256390
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2022-01-08 14:44:53 +0000

    Clean up comments

--- files/grest/cron/jobs/active-stake-cache-update.sh
+++ files/grest/cron/jobs/active-stake-cache-update.sh
@@ -8,8 +8,7 @@ echo "$(date +%F_%H:%M:%S) Running active stake cache update..."
   echo "No update needed, exiting..." &&
   exit 0;
 
-# 2nd and 3rd number in the logs correspond to number of stakes and epoch number
-# Could break due to upstream changes on db-sync
+# This could break due to upstream changes on db-sync (based on log format)
 last_epoch_stakes_log=$(grep -r 'Handling.*.stakes for epoch ' "$(ls -1rt "$(dirname "$0")"/../../logs/dbsync.json "$(dirname "$0")"/../../logs/archive/dbsync-*.json 2>/dev/null)" /dev/null | sed -e 's#.*.Handling ##' -e 's#stakes for epoch##' -e 's# slot .*.$##' | tail -1)
 [[ -z ${last_epoch_stakes_log} ]] &&
   echo "Could not find any 'Handling stakes' log entries, exiting..." &&
@@ -23,16 +22,16 @@ db_last_epoch_no=$(psql ${DB_NAME} -qbt -c "SELECT grest.get_current_epoch();" |
   echo "Mismatch between last epoch in logs and database, exiting..." &&
   exit 1;
 
-# count current epoch entries
+# Count current epoch entries processed by db-sync
 db_epoch_stakes_count=$(psql ${DB_NAME} -qbt -c "SELECT grest.get_epoch_stakes_count(${db_last_epoch_no});" | tr -cd '[:alnum:]')
 
-# check if db-sync completed handling stakes
+# Check if db-sync completed handling stakes
 [[ "${db_epoch_stakes_count}" != "${logs_last_epoch_stakes_count}" ]] &&
   echo "Logs last epoch stakes count: ${logs_last_epoch_stakes_count}" &&
   echo "DB last epoch stakes count: ${db_epoch_stakes_count}" &&
   echo "db-sync stakes handling still incomplete, exiting..." &&
   exit 0;
 
-# If we get this far, it means stakes have been validated, so we run the cache update
+# Stakes have been validated, run the cache update
 psql ${DB_NAME} -qbt -c "SELECT GREST.active_stake_cache_update(${db_last_epoch_no});" 2>&1 1>/dev/null
 echo "$(date +%F_%H:%M:%S) Job done!"
