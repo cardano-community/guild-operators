36f33cd72c8c95c0cc4259c8deb09f306ef70738
Ola <ola.ahlman@gmail.com>
2022-01-10 11:01:17 +0100

    address_info upd to include tokens + lovelace stringify

--- files/grest/rpc/address/address_info.sql
+++ files/grest/rpc/address/address_info.sql
@@ -2,43 +2,44 @@ DROP FUNCTION IF EXISTS grest.address_info (text);
 
 CREATE FUNCTION grest.address_info (_address text DEFAULT NULL)
   RETURNS TABLE (
-    balance numeric,
-    stake_address text,
+    balance text,
+    stake_address character varying,
     utxo_set json)
   LANGUAGE PLPGSQL
   AS $$
-DECLARE
-  _stake_address text default NULL;
-  _stake_address_id bigint default NULL;
 BEGIN
-  SELECT
-    stake_address_id
-  FROM
-    tx_out
-  WHERE
-    address = _address
-  LIMIT 1 INTO _stake_address_id;
-  IF _stake_address_id IS NOT NULL THEN
-    SELECT
-      view
-    FROM
-      stake_address
-    WHERE
-      id = _stake_address_id INTO _stake_address;
-  END IF;
   RETURN QUERY
   SELECT
-    SUM(tx_out.value),
-    _stake_address,
-    JSON_AGG(JSON_BUILD_OBJECT('tx_hash', ENCODE(tx.hash, 'hex'), 'tx_index', tx_out.index, 'value', tx_out.value)) utxo_set
+    SUM(tx_out.value)::text,
+    SA.view,
+    JSON_AGG(JSON_BUILD_OBJECT(
+    'tx_hash', ENCODE(tx.hash, 'hex'), 
+    'tx_index', tx_out.index, 
+    'value', tx_out.value::text,
+    'asset_list', COALESCE((
+      SELECT 
+        JSON_AGG(JSON_BUILD_OBJECT(
+          'policy_id', ENCODE(MA.policy, 'hex'),
+          'asset_name', ENCODE(MA.name, 'hex'),
+          'quantity', MTX.quantity::text
+          ))
+      FROM 
+          ma_tx_out MTX
+          INNER JOIN multi_asset MA ON MA.id = MTX.ident
+      WHERE 
+          MTX.tx_out_id = tx_out.id
+    ), JSON_BUILD_ARRAY())
+  )) utxo_set
   FROM
     public.tx_out
     INNER JOIN public.tx ON tx_out.tx_id = tx.id
     LEFT JOIN public.tx_in ON tx_in.tx_out_id = tx_out.tx_id
       AND tx_in.tx_out_index = tx_out.index
+    LEFT JOIN stake_address SA on tx_out.stake_address_id = SA.id
   WHERE
     tx_in.id IS NULL
-    AND tx_out.address = _address;
+    AND tx_out.address = _address
+  GROUP BY SA.view;
 END;
 $$;
 
