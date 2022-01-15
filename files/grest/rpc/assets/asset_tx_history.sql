f3f02f1aebcafddbe07a1bc8c5e0071ffe85db3f
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2022-01-15 20:59:37 +0000

    asset_tx_history rpc

---
+++ files/grest/rpc/assets/asset_tx_history.sql
@@ -0,0 +1,43 @@
+DROP FUNCTION IF EXISTS grest.asset_tx_history (text, text);
+
+CREATE FUNCTION grest.asset_tx_history (_asset_policy text, _asset_name text)
+  RETURNS TABLE (
+    policy_id text,
+    asset_name text,
+    tx_hashes text[]
+  )
+  LANGUAGE PLPGSQL
+  AS $$
+DECLARE
+  _asset_policy_decoded bytea;
+  _asset_name_decoded bytea;
+  _asset_id int;
+BEGIN
+  SELECT DECODE(_asset_policy, 'hex') INTO _asset_policy_decoded;
+  SELECT DECODE(_asset_name, 'hex') INTO _asset_name_decoded;
+  SELECT id INTO _asset_id FROM multi_asset MA WHERE MA.policy = _asset_policy_decoded AND MA.name = _asset_name_decoded;
+
+  RETURN QUERY
+    SELECT
+      _asset_policy,
+      _asset_name,
+      array_agg(
+        ENCODE(tx_hashes.hash, 'hex')
+          ORDER BY tx_hashes.id
+      )
+    FROM (
+      SELECT DISTINCT ON (tx.hash)
+        tx.id,
+        tx.hash
+      FROM
+        ma_tx_out MTO
+        INNER JOIN tx_out TXO ON TXO.id = MTO.tx_out_id
+        INNER JOIN tx ON tx.id = TXO.tx_id
+      WHERE
+        MTO.ident = _asset_id
+      GROUP BY
+        ident,
+        tx.id
+    ) tx_hashes;
+END;
+$$;
