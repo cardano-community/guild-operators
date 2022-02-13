cfe7ca4467d932046b45df8ea0813a7ad548d130
Ola [AHLNET] <ola.ahlman@gmail.com>
2022-02-13 22:08:39 +0100

    Two minor koios api endpoint fixes. (#1326)
    * Two minor koios api endpoint fixes.
    
    - Hex encoding in asset_list view
    - Stringify total_supply in asset_policy_info
    
    * Doc fixes
    
    Co-authored-by: KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>

--- files/grest/rpc/views/asset_list.sql
+++ files/grest/rpc/views/asset_list.sql
@@ -4,7 +4,7 @@ CREATE VIEW grest.asset_list AS
   SELECT
       ENCODE(policy, 'hex') AS policy_id,
       JSON_BUILD_OBJECT(
-        'hex', JSON_AGG(name),
+        'hex', JSON_AGG(ENCODE(name, 'hex')),
         'ascii', JSON_AGG(ENCODE(name, 'escape'))
       ) AS asset_names
   FROM 
