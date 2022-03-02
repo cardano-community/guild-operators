fc710b3ded779452ed89b8c848b79b733f88d75f
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2022-03-02 09:43:53 +0000

    clean up

--- files/grest/rpc/epoch/epoch_info.sql
+++ files/grest/rpc/epoch/epoch_info.sql
@@ -49,30 +49,3 @@ $$;
 
 COMMENT ON FUNCTION grest.epoch_info IS 'Get the epoch information, all epochs if no epoch specified';
 
-
--- Helper function for calculating theoretical epoch start/end times
-DROP FUNCTION IF EXISTS grest.tmp_epoch_trans(numeric);
-
-CREATE FUNCTION grest.tmp_epoch_trans(_epoch_no NUMERIC)
-  RETURNS TABLE (
-      start_time timestamp without time zone,
-      end_time timestamp without time zone
-    )
-    LANGUAGE PLPGSQL
-  AS $$
-DECLARE
-  shelley_epoch_duration numeric := (select epochlength::numeric * slotlength::numeric as epochduration from grest.genesis);
-  shelley_ref_epoch numeric := (select epoch_no from epoch_param ORDER BY epoch_no LIMIT 1);
-  shelley_ref_time timestamp := (select i_first_block_time from grest.epoch_info_cache where epoch_no = shelley_ref_epoch);
-BEGIN
-  IF _epoch_no < shelley_ref_epoch THEN
-    RETURN QUERY (
-      SELECT (select e.start_time from epoch e where no = _epoch_no) AS start_time, (select e.start_time from epoch e where no = _epoch_no + 1 ) AS end_time
-    );
-  ELSE
-    RETURN QUERY (
-      SELECT (shelley_ref_time + ((_epoch_no - shelley_ref_epoch) * shelley_epoch_duration)::text::interval) AS start_time, (shelley_ref_time + (((_epoch_no+1) - shelley_ref_epoch) * shelley_epoch_duration)::text::interval) AS end_time 
-    );
-  END IF;
-END;
-$$;
