3d53c79bfdcea8007017530d6a8e37773dfd01a4
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2022-08-20 12:04:48 +0100

    Fix address_assets to return one row per address even if multiple asset policies owned

--- files/grest/rpc/address/address_assets.sql
+++ files/grest/rpc/address/address_assets.sql
@@ -17,8 +17,8 @@ BEGIN
     FROM
       MA_TX_OUT MTX
       INNER JOIN MULTI_ASSET MA ON MA.id = MTX.ident
-      INNER JOIN TX_OUT TXO on TXO.ID = MTX.TX_OUT_ID
-      LEFT JOIN TX_IN on TXO.TX_ID = TX_IN.TX_OUT_ID
+      INNER JOIN TX_OUT TXO ON TXO.ID = MTX.TX_OUT_ID
+      LEFT JOIN TX_IN ON TXO.TX_ID = TX_IN.TX_OUT_ID
         AND TXO.INDEX::smallint = TX_IN.TX_OUT_INDEX::smallint
     WHERE
       TXO.address = ANY(_addresses)
@@ -28,18 +28,28 @@ BEGIN
   )
 
   SELECT
-    aa.address,
-    JSON_BUILD_OBJECT(
-      'policy_id', ENCODE(aa.policy, 'hex'),
-      'assets', JSON_AGG( JSON_BUILD_OBJECT(
-        'asset_name', ENCODE(aa.name, 'hex'),
-        'balance', aa.quantity::text)
-      )
-    )
-  FROM
-    _all_assets aa
+    assets_grouped.address,
+    JSON_AGG(assets_grouped.assets)
+  FROM (
+    SELECT
+      aa.address,
+      JSON_BUILD_OBJECT(
+        'policy_id', ENCODE(aa.policy, 'hex'),
+        'assets', JSON_AGG(
+          JSON_BUILD_OBJECT(
+            'asset_name', ENCODE(aa.name, 'hex'),
+            'asset_name_ascii', ENCODE(aa.name, 'escape'),
+            'balance', aa.quantity::text
+          )
+        )
+      ) as assets
+    FROM 
+      _all_assets aa
+    GROUP BY
+      aa.address, aa.policy
+  ) assets_grouped
   GROUP BY
-    aa.address, aa.policy;
+    assets_grouped.address;
 END;
 $$;
 
