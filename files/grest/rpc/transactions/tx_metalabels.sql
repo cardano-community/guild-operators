3da294d5128668f5f261250556bf3b630c7e08c2
rdlrt <3169068+rdlrt@users.noreply.github.com>
2022-09-25 23:16:19 +1000

    Fix tx_metalabels casting, closes koios-artifacts#95

--- files/grest/rpc/transactions/tx_metalabels.sql
+++ files/grest/rpc/transactions/tx_metalabels.sql
@@ -7,13 +7,13 @@ CREATE FUNCTION grest.tx_metalabels()
 BEGIN
   RETURN QUERY
   WITH RECURSIVE t AS (
-    (SELECT tm.key::text FROM public.tx_metadata tm ORDER BY key LIMIT 1) 
+    (SELECT tm.key FROM public.tx_metadata tm ORDER BY key LIMIT 1)
     UNION ALL
     SELECT (SELECT tm.key FROM tx_metadata tm WHERE tm.key > t.key ORDER BY key LIMIT 1)
     FROM t
       WHERE t.key IS NOT NULL
   )
-  SELECT t.key FROM t WHERE t.key IS NOT NULL;
+  SELECT t.key::text FROM t WHERE t.key IS NOT NULL;
 END;
 $$;
 
