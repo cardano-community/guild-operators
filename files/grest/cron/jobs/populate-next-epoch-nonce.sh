22f320700d934ee162def0b1a5242d46241fa620
Damjan Ostrelic <damjan.ostrelic@gmail.com>
2022-10-26 11:01:16 +0100

    Add string cast for PSQL

--- files/grest/cron/jobs/populate-next-epoch-nonce.sh
+++ files/grest/cron/jobs/populate-next-epoch-nonce.sh
@@ -15,6 +15,6 @@ next_epoch=$((current_epoch + 1))
 
 [[ ${current_slot_in_epoch} -ge ${min_slot} ]] &&
   next_epoch_nonce=$(echo "$(${CCLI_PATH} query protocol-state --testnet-magic "${NWMAGIC}" | jq -r .candidateNonce.contents)$(${CCLI_PATH} query protocol-state --testnet-magic "${NWMAGIC}" | jq -r .lastEpochBlockNonce.contents)" | xxd -r -p | b2sum -b -l 256 | awk '{print $1}') &&
-  psql ${DB_NAME} -c "INSERT INTO grest.epoch_info_cache (epoch_no, p_nonce) VALUES (${next_epoch}, ${next_epoch_nonce}) ON CONFLICT DO NOTHING;"
+  psql ${DB_NAME} -c "INSERT INTO grest.epoch_info_cache (epoch_no, p_nonce) VALUES (${next_epoch}, '${next_epoch_nonce}') ON CONFLICT DO NOTHING;"
 
 echo "$(date +%F_%H:%M:%S) Job done!"
