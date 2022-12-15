c6602eb4690bfaa8bdfa6bf7930de4131277026d
Ola <ola.ahlman@gmail.com>
2022-12-15 23:18:02 +0100

    escape fix

--- scripts/grest-helper-scripts/setup-grest.sh
+++ scripts/grest-helper-scripts/setup-grest.sh
@@ -108,7 +108,7 @@ SGVERSION=1.0.9
     local cron_pattern=$2
     local cron_job_path="${CRON_DIR}/${CNODE_VNAME}-${job}"
     local cron_scripts_path="${CRON_SCRIPTS_DIR}/${job}.sh"
-    local cron_log_path="${LOG_DIR}/${job}_`date +%d%m%y`.log"
+    local cron_log_path="${LOG_DIR}/${job}_\`date +%d%m%y\`.log"
     local cron_job_entry="${cron_pattern} ${USER} /bin/bash ${cron_scripts_path} >> ${cron_log_path} 2>&1"
     remove_cron_job "${job}"
     sudo bash -c "{ echo '${cron_job_entry}'; } > ${cron_job_path}"
