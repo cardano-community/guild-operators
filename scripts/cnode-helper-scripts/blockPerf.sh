9fa33813054ac7e84529fabbd280f1d9b3ea8f3e
Markus <gufmar@gmail.com>
2022-12-30 00:56:11 +0100

    div fixes and additions
    - cntools update-check mechanism
    - fix checkFixConfig boolean value setting
    - add guild testnet support
    - enhance manual parsing
    - parse and report header announcing IP:port
    - increase logLinecycles limit (extended tracers config)
    - no hash parsed handling (wip)

--- scripts/cnode-helper-scripts/blockPerf.sh
+++ scripts/cnode-helper-scripts/blockPerf.sh
@@ -12,8 +12,9 @@
 #CONFIG=""             # Outside the cnTools environment you can manually point this script to your nodes config.json file
 #SELFISH_MODE='Y'      # in case you don't want to share this node's block propagation data, turn the selfish mode on (Y)
 #SERVICE_MODE='N'      # if you deploy-as-service this script it will run with the -s (service) parameter, and surpress console/syslog output. you can overwrite it here, restart the service and watch the console output with 'journalctl -f -u cnode-tu-blockperf'
+#BATCH_AUTO_UPDATE=N   # Set to Y to automatically update the script if a new version is available without user interaction
 
-#CNODE_PORT=6000       # the port on which this node runs (automatically read from cnTools:env   outside cnTools you need to manually set this parameter
+#CNODE_PORT=6010       # the port on which this node runs (automatically read from cnTools:env   outside cnTools you need to manually set this parameter)
 
 #AddrBlacklist="192.168.1.123, " # uncomment with your block producers or other nodes IP that you do not want to expose to common view
 
@@ -21,7 +22,10 @@
 # Do NOT modify code below           #
 ######################################
 
-BP_VERSION=v1.3.4
+BP_VERSION=v1.3.5
+
+SKIP_UPDATE=N
+[[ $1 = "-u" ]] && SKIP_UPDATE=Y && shift
 
 deploy_systemd() {
   echo "Deploying ${CNODE_VNAME} blockPerf as systemd service.."
@@ -57,8 +61,10 @@ usage() {
 		Usage: $(basename "$0") [-d] [-s]
 		
 		Cardano Node wrapper script !!
+		-u    Skip script update check overriding UPDATE_CHECK value in env (must be first argument to script)
 		-d    Deploy cnode-tu-blockperf as a systemd service
 		-s    Run cnode-tu-blockperf without INFO message output to console/syslog 
+		-m    Run in manual mode, by specifying a blockNum and logfile (no online reporting, just console output)
 		
 		EOF
   exit 1
@@ -80,6 +86,39 @@ done
 if [ -z "$CONFIG" ]; then
   # in CNTools environments just let the script determine config, logfile, parameters
   [[ -f "$(dirname $0)"/env ]] &&  . "$(dirname $0)"/env offline
+
+  if [[ ${UPDATE_CHECK} = Y && ${SKIP_UPDATE} != Y && ${SERVICE_MODE} != Y ]]; then
+
+    echo "Checking for script updates..."
+
+    # Check availability of checkUpdate function
+    if [[ ! $(command -v checkUpdate) ]]; then
+      echo -e "\nCould not find checkUpdate function in env, make sure you're using official guild docos for installation!"
+      exit 1
+    fi
+
+    # check for env update
+    ENV_UPDATED=${BATCH_AUTO_UPDATE}
+    checkUpdate "${PARENT}"/env N N N
+    case $? in
+      1) ENV_UPDATED=Y ;;
+      2) exit 1 ;;
+    esac
+
+    # check for blockPerf.sh update
+    checkUpdate "${PARENT}"/blockPerf.sh ${ENV_UPDATED}
+    case $? in
+      1) $0 "-u" "$@"; exit 0 ;; # re-launch script with same args skipping update check
+      2) exit 1 ;;
+    esac
+  fi
+
+  # source common env variables in case it was updated
+  until . "${PARENT}"/env; do
+    echo "sleeping for 10s and testing again..."
+    sleep 10
+  done
+
 else 
   # parse the min required values from the specified CONFIG file  
   if ! GENESIS_JSON=$(jq -er '.ShelleyGenesisFile' "${CONFIG}" 2>/dev/null); then
@@ -96,7 +135,7 @@ checkFixConfig() {
   checkKey=${1}; 
   checkVal=$2;
   if [[  "$3" == "fix" ]]; then
-    [[ "$(jq -r ".${checkKey}" "${CONFIG}")" != "${checkVal}" ]] && jq --argjson theKey $checkKey --arg theVal $checkVal 'setpath($theKey; $theVal)' ${CONFIG} > ${CONFIG}.tmp && mv ${CONFIG}.tmp ${CONFIG} && echo "INFO: setting .$checkKey to ${checkVal}" && config_change=1
+    [[ "$(jq -r ".${checkKey}" "${CONFIG}")" != "${checkVal}" ]] && jq --argjson theKey $checkKey --argjson theVal $checkVal 'setpath($theKey; $theVal)' ${CONFIG} > ${CONFIG}.tmp && mv ${CONFIG}.tmp ${CONFIG} && echo "INFO: setting .$checkKey to ${checkVal}" && config_change=1
   else  # alert only
     [[ "$(jq -r ".${checkKey}" "${CONFIG}")" != "${checkVal}" ]] && echo "INFO: for blockPerf parsing please set ${checkKey[0]}:\"${checkVal}\" in ${CONFIG}" && config_change=1
   fi
@@ -105,8 +144,8 @@ checkFixConfig() {
 #Deploy systemd if -d argument was specified
 if [[ "${DEPLOY_SYSTEMD}" == "Y" ]]; then
   # if not already enabled activate the required Tracers in the config file
-  checkFixConfig '["TraceChainSyncClient"]' "true" "fix";
-  checkFixConfig '["TraceBlockFetchClient"]' "true" "fix";
+  checkFixConfig '["TraceChainSyncClient"]' true "fix";
+  checkFixConfig '["TraceBlockFetchClient"]' true "fix";
   checkFixConfig '["TracingVerbosity"]' "NormalVerbosity" "fix";
   [[ $config_change -eq 1 ]] && echo "Please restart the node with new Tracers before using blockPerf."
   deploy_systemd && exit 0
@@ -122,8 +161,8 @@ if [[ "${CONFIG##*.}" = "json" ]] && [[ -f ${CONFIG} ]]; then
   [[ -z ${EKG_PORT} ]] && EKG_PORT=$(jq .hasEKG $CONFIG)
   [[ -z "${EKG_PORT}" ]] && echo -e "ERROR: Failed to locate the EKG Port in node configuration file" && errors=1
   NWMAGIC=$(jq -r .networkMagic < ${GENESIS_JSON})
-  checkFixConfig '["TraceChainSyncClient"]' "true" "alert";
-  checkFixConfig '["TraceBlockFetchClient"]' "true" "alert";
+  checkFixConfig '["TraceChainSyncClient"]' true "alert";
+  checkFixConfig '["TraceBlockFetchClient"]' true "alert";
   checkFixConfig '["TracingVerbosity"]' "NormalVerbosity" "alert";
   [[ $config_change -eq 1 ]] && exit 1
 else 
@@ -138,6 +177,9 @@ case ${NWMAGIC} in
   2) 
     [[ -z ${NETWORK_NAME} ]] && NETWORK_NAME="PreView"
     NETWORK_UTIME_OFFSET=1655683200;;
+  141) 
+    [[ -z ${NETWORK_NAME} ]] && NETWORK_NAME="Guild-Testnet"
+    NETWORK_UTIME_OFFSET=1639089874;;
   764824073) 
     [[ -z ${NETWORK_NAME} ]] && NETWORK_NAME="Mainnet"
     NETWORK_UTIME_OFFSET=1591566291;; 
@@ -147,7 +189,7 @@ esac
 
 # check if the script is not already running (service and console) 
 pidfile=${CNODE_HOME}/blockPerf-running.pid
-if [[ -f ${pidfile} ]]; then
+if [[ -f ${pidfile} && "${PARSE_MANUAL}" != "Y" ]]; then
     echo "WARN: This script is already running on this node for ${NETWORK_NAME} network (probably as a service)" && exit 1
 else
     trap "rm -f -- '$pidfile'" EXIT
@@ -176,18 +218,18 @@ reportBlock() {
             line_tsv=$(jq -r '[
              .at //0,
              .data.slot //0,
-             .data.block //0
+             .data.peer.remote.addr //0,
+             .data.peer.remote.port //0
              ] | @tsv' <<< "${blockLogLine}")
             read -ra line_data_arr <<< ${line_tsv}
             [ -z "$blockTimeTbh" ] && blockTimeTbh=$(date -d ${line_data_arr[0]} +"%F %T,%3N" || 0)
             [ -z "$blockSlot" ] && blockSlot=${line_data_arr[1]}
+            [ -z "$blockTimeTbhAddr" ] && blockTimeTbhAddr=${line_data_arr[2]}
+            [ -z "$blockTimeTbhPort" ] && blockTimeTbhPort=${line_data_arr[3]}
             [ -z "$blockSlotTime" ] && blockSlotTime=$(getSlotDate ${blockSlot})
             missingTbh=false
           fi
           ;;
-        #SendFetchRequest)
-        #  [ -z "$blockTimeSfr1" ] && blockTimeSfr1=$(date -d $(jq -r .at <<< $blockLogLine) +"%F %T,%3N" || 0)
-        #  ;;
         CompletedBlockFetch)
           if $missingCbf; then
             line_tsv=$(jq -r '[
@@ -220,7 +262,7 @@ reportBlock() {
           ;;
       esac;
       blockLogLineCycles=$((blockLogLineCycles+1))
-      if [[ "$blockLogLineCycles" -gt 100 ]]; then 
+      if [[ "$blockLogLineCycles" -gt 300 ]]; then 
         # security escape after x loglines / better safe than sorry
         echo "WARN: blockheight:${iblockHeight} (leave loglines loop)" 
         break
@@ -239,6 +281,13 @@ reportBlock() {
     [ ! -z "$blockTimeAb" ] && deltaCbfAb=$(( $(getDeltaMS ${blockTimeAb} ${blockSlotTime},000) - deltaSfrCbf - deltaTbhSfr - deltaSlotTbh)) || deltaCbfAb="NULL"
     [[ "$deltaCbfAb" -lt 0 ]] && deltaCbfAb=0  # rare cases of ab logged before cbf (can be removed after fixed in node )
     # may blacklist some internal IPs, to not expose them to common views (api.clio.one)
+    if [[ "$AddrBlacklist" == *"$blockTimeTbhAddr"* ]]; then
+      blockTimeTbhAddrPublic="0.0.0.0"
+      blockTimeTbhPortPublic="0"
+    else
+      blockTimeTbhAddrPublic=$blockTimeTbhAddr
+      blockTimeTbhPortPublic=$blockTimeTbhPort
+    fi
     if [[ "$AddrBlacklist" == *"$blockTimeCbfAddr"* ]]; then
       blockTimeCbfAddrPublic="0.0.0.0"
       blockTimeCbfPortPublic="0"
@@ -252,11 +301,11 @@ reportBlock() {
       echo -e "WARN: blockheight:${iblockHeight} (negative delta) \n  tbh:${blockTimeTbh} ${deltaSlotTbh}\n  sfr:${blockTimeSfrX} ${deltaTbhSfr}\n  cbf:${blockTimeCbf} ${deltaSfrCbf}\n  ab:${blockTimeAb} ${deltaCbfAb}" 
       echo -e "DBG \n blockTimeCbfAddr: $blockTimeCbfAddr \n blockTimeCbfPort: $blockTimeCbfPort \n sbx: $sbx \n line_tsv: $line_tsv"
       #Debug: look into when and why this happens
-      echo -e "blockheight:${iblockHeight} (negative delta) \n  tbh:${blockTimeTbh} ${deltaSlotTbh}\n  sfr:${blockTimeSfrX} ${deltaTbhSfr}\n  cbf:${blockTimeCbf} ${deltaSfrCbf}\n  ab:${blockTimeAb} ${deltaCbfAb} \n blockTimeCbfAddr: $blockTimeCbfAddr \n blockTimeCbfPort: $blockTimeCbfPort \n sbx: $sbx \n line_tsv: $line_tsv \n \n blockLogLine: \n${blockLogLine} \n\n ${blockLog}" > /opt/cardano/cnode/zzz_debug_WARN_block_${iblockHeight}.json
+      #echo -e "blockheight:${iblockHeight} (negative delta) \n  tbh:${blockTimeTbh} ${deltaSlotTbh}\n  sfr:${blockTimeSfrX} ${deltaTbhSfr}\n  cbf:${blockTimeCbf} ${deltaSfrCbf}\n  ab:${blockTimeAb} ${deltaCbfAb} \n blockTimeCbfAddr: $blockTimeCbfAddr \n blockTimeCbfPort: $blockTimeCbfPort \n sbx: $sbx \n line_tsv: $line_tsv \n \n blockLogLine: \n${blockLogLine} \n\n ${blockLog}" > zzz_debug_WARN_block_${iblockHeight}.json
     else
       if [[ "${deltaSlotTbh}" -lt 10000 ]] && [[ "$((blockSlot-slotHeightPrev))" -lt 200 ]]; then
-        [[ ${SELFISH_MODE} != "Y" ]] && result=$(curl -4 -s "https://api.clio.one/blocklog/v1/?magic=${NWMAGIC}&bpv=${BP_VERSION}&nport=${CNODE_PORT}&bn=${iblockHeight}&slot=${blockSlot}&tbh=${deltaSlotTbh}&sfr=${deltaTbhSfr}&cbf=${deltaSfrCbf}&ab=${deltaCbfAb}&g=${blockTimeG}&size=${blockSize}&addr=${blockTimeCbfAddrPublic}&port=${blockTimeCbfPortPublic}&bh=${blockHash}&bpenv=${envBP}" &)
-        [[ ${SERVICE_MODE} != "Y" ]] && echo -e "${FG_YELLOW}Block:.... ${iblockHeight} ( ${blockHash:0:10} ...)\n${NC} Slot..... ${blockSlot} ($((blockSlot-slotHeightPrev))s)\n ......... ${blockSlotTime}\n Header... ${blockTimeTbh} (+${deltaSlotTbh} ms)\n RequestX. ${blockTimeSfrX} (+${deltaTbhSfr} ms)\n Block.... ${blockTimeCbf} (+${deltaSfrCbf} ms)\n Adopted.. ${blockTimeAb} (+${deltaCbfAb} ms)\n Size..... ${blockSize} bytes\n delay.... ${blockDelay} sec\n From..... ${blockTimeCbfAddr}:${blockTimeCbfPort}"
+        [[ ${SELFISH_MODE} != "Y" ]] && result=$(curl -4 -s "https://api.clio.one/blocklog/v1/?magic=${NWMAGIC}&bpv=${BP_VERSION}&nport=${CNODE_PORT}&bn=${iblockHeight}&slot=${blockSlot}&tbh=${deltaSlotTbh}&tbhAddr=${blockTimeTbhAddrPublic}&tbhPort=${blockTimeTbhPortPublic}&sfr=${deltaTbhSfr}&cbf=${deltaSfrCbf}&ab=${deltaCbfAb}&g=${blockTimeG}&size=${blockSize}&addr=${blockTimeCbfAddrPublic}&port=${blockTimeCbfPortPublic}&bh=${blockHash}&bpenv=${envBP}" &)
+        [[ ${SERVICE_MODE} != "Y" ]] && echo -e "${FG_YELLOW}Block:.... ${iblockHeight} ( ${blockHash:0:10} ...)\n${NC} Slot..... ${blockSlot} ($((blockSlot-slotHeightPrev))s)\n ......... ${blockSlotTime}\n Header... ${blockTimeTbh} (+${deltaSlotTbh} ms) from ${blockTimeTbhAddr}:${blockTimeTbhPort}\n RequestX. ${blockTimeSfrX} (+${deltaTbhSfr} ms)\n Block.... ${blockTimeCbf} (+${deltaSfrCbf} ms) from ${blockTimeCbfAddr}:${blockTimeCbfPort}\n Adopted.. ${blockTimeAb} (+${deltaCbfAb} ms)\n Size..... ${blockSize} bytes\n delay.... ${blockDelay} sec"
       else
         # skip block reporting while node is synching up
         [[ ${SERVICE_MODE} != "Y" ]] && echo -e "${FG_YELLOW}Block:.... ${iblockHeight} skipped\n${NC} Slot..... ${blockSlot}\n ......... ${blockSlotTime}\n now...... $(date +"%F %T")"
@@ -281,16 +330,19 @@ if [[ "${PARSE_MANUAL}" == "Y" ]]; then
     echo " looking for block $2"
     echo " in logfile $3"
     blockHash=$(grep -m 1 "$iblockHeight" ${logfile} | jq -r .data.block)
-    blockLog=$(grep -E ${blockHash:0:10} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch' )
-	reportBlock ${blockLog} && exit 0;
+    echo "DBG Hash: $blockHash"
+    blockLog=$(grep -E ${blockHash:0:10} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' )
+    echo "DBG Loglines: $(echo $blockLog | grep -c ":")"
+    reportBlock ${blockLog} && exit 0;
     exit 1
   else
     echo "in manual parse mode (-m) please specify [blockheight intNum] and [logfile path]"
-	exit 1
+    exit 1
   fi
 fi
 
-echo "INFO parsing ${logfile} for ${NETWORK_NAME} blocks (networkmagic: ${NWMAGIC})"
+echo "blockPerf $BP_VERSION"
+echo "parsing ${logfile} for ${NETWORK_NAME} blocks (networkmagic: ${NWMAGIC})"
 
 # on (re)start wait until node metrics become available
 while true [ -z $(curl -s -H 'Accept: application/json' http://${EKG_HOST}:${EKG_PORT}/ | jq -r '.cardano.node.metrics.blockNum.int.val //0') ]
@@ -329,12 +381,18 @@ do
     for (( iblockHeight=$blockHeightPrev+1; iblockHeight<=$blockHeight; iblockHeight++ ))
     do  #catch up from previous to current blockheight
       blockHash=$(grep -m 1 "$iblockHeight" ${logfile} | jq -r .data.block)
-      blockLog=$(grep -E ${blockHash:0:10} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' )
-      if [ $(echo "$blockLog" | grep -h -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' | jq -r .data.kind | sort | uniq | wc -l) -lt 4 ]; then 
-        # grep'ed blockLog is incomplete (4 steps) probably because of log rotation. so let's grep from all logs
-        blockLog=$(grep -h -E ${blockHash:0:10} ${logfile/.json/-*} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' )
+      if [ ! -z $blockHash ]; then
+        blockLog=$(grep ":${blockHash:0:10}" ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' )
+        if [ $(echo "$blockLog" | grep -h -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' | jq -r .data.kind | sort | uniq | wc -l) -lt 4 ]; then 
+          # grep'ed blockLog is incomplete (4 steps) probably because of log rotation. so let's grep from all logs
+          blockLog=$(grep -h -E ${blockHash:0:10} ${logfile/.json/-*} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' )
+        fi
+        reportBlock ${blockLog};
+       else
+        echo -e "WARN: blockheight:${iblockHeight} no hash" 
+        #Debug: look into when and why this happens
+        #echo -e "blockheight:${iblockHeight} (no hash) \n\n $(grep -m 1 "$iblockHeight" ${logfile})" > zzz_debug_WARN_block_${iblockHeight}.json
       fi
-      reportBlock ${blockLog};
       echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     done  # catch up from previous to current blockheight
     blockHeightPrev=$blockHeight; 
@@ -346,7 +404,7 @@ do
       blockHash=$(grep -m 1 "${blockHash}+.*$slotNum" ${logfile} | jq -r .data.block)
     fi
     if [ ! -z $blockHash ]; then
-      blockLog=$(grep ${blockHash} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork')
+      blockLog=$(grep ":${blockHash}" ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork')
       if [ $(echo "$blockLog" | grep -h -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' | jq -r .data.kind | sort | uniq | wc -l) -lt 4 ]; then 
         # grep'ed blockLog is incomplete (4 steps) probably because of log rotation. so let's grep from all logs
         blockLog=$(grep -h -E ${blockHash:0:10} ${logfile/.json/-*} ${logfile} | grep -E 'TraceDownloadedHeader|SendFetchRequest|CompletedBlockFetch|AddedToCurrentChain|SwitchedToAFork' )
@@ -355,6 +413,10 @@ do
       iblockHeight=$(jq -r .data.blockNo.unBlockNo <<< "${iblockHeight}")
       echo "b:${iblockHeight}	s:${slotNum}" >> forks.log
       reportBlock ${blockLog};
+    else
+      echo -e "WARN: blockheight:${iblockHeight} no hash" 
+      #Debug: look into when and why this happens
+      #echo -e "blockheight:${iblockHeight} (no hash) \n\n $(grep -m 1 "$iblockHeight" ${logfile})" > zzz_debug_WARN_block_${iblockHeight}.json
     fi
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     forksPrev=$forks; 
