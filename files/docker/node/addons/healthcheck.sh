26b36957b93e1ed265eba05d32258e0178fdc4c9
illuminatus <trevor.benson@gmail.com>
2023-04-22 00:37:13 -0700

    Enable KOIOS in healthcheck.sh (#1627)
    * Update guild-deploy.sh for cncli 5.3.2 download compiled with musl (#1626)
    
    Update guild-deploy.sh
    
    Changed cncli download parser to linux-musl from linux-gnu. Andrew obviously changed the compiler from standard GNU to MUSL with cncli 5.2.3
    
    * Enable KOIOS in healthcheck.sh
    
    ---------
    
    Co-authored-by: Straightpool <42584250+Straightpool@users.noreply.github.com>
    Co-authored-by: RdLrT <3169068+rdlrt@users.noreply.github.com>

--- files/docker/node/addons/healthcheck.sh
+++ files/docker/node/addons/healthcheck.sh
@@ -10,14 +10,34 @@ if [[ "$NETWORK" == "guild-mainnet" ]]; then NETWORK=mainnet; fi
 
 FIRST=$($CCLI query tip --testnet-magic ${NWMAGIC} | jq .block)
 
-sleep 60;
-
-SECOND=$($CCLI query tip --testnet-magic ${NWMAGIC} | jq .block)
-
-
-if [[ "$FIRST" -ge "$SECOND" ]]; then
-echo "there is a problem";
-exit 1
+if [[ "${ENABLE_KOIOS}" == "N" ]] || [[ -z "${KOIOS_API}" ]]; then
+    # when KOIOS is not enabled or KOIOS_API is unset, use default behavior
+    sleep 60;
+    SECOND=$($CCLI query tip --testnet-magic ${NWMAGIC} | jq .block)
+    if [[ "$FIRST" -ge "$SECOND" ]]; then
+        echo "there is a problem"
+        exit 1
+    else
+        echo "we're healthy - node: $FIRST -> node: $SECOND"
+    fi
 else
-echo "we're healthy - $FIRST -> $SECOND"
+    # else leverage koios and only require the node is on tip
+    CURL=$(which curl)
+    JQ=$(which jq)
+    URL="${KOIOS_API}/tip"
+    SECOND=$($CURL -s "${URL}" | $JQ '.[0].block_no')
+    for (( CHECK=1; CHECK<=20; CHECK++ )); do
+        if [[ "$FIRST" -eq "$SECOND" ]]; then
+            echo "we're healthy - node: $FIRST == koios: $SECOND"
+            exit 0
+        elif [[ "$FIRST" -lt "$SECOND" ]]; then
+            sleep 3
+            FIRST=$($CCLI query tip --testnet-magic ${NWMAGIC} | jq .block)
+        elif [[ "$FIRST" -gt "$SECOND" ]]; then
+            sleep 3
+            SECOND=$($CURL "${KOIOS_URL}" | $JQ '.[0].block_no')
+        fi
+    done
+    echo "there is a problem"
+    exit 1
 fi
