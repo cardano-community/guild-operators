ff616e139158ea615a42e589bea49234b2a3c988
Benjamin <31835324+Fuma419@users.noreply.github.com>
2024-04-12 17:32:01 -0400

    Revert "Update guild-deploy.sh"
    This reverts commit 986a091ad38ce455c14c1ff3bf253f402e15d5c2.

--- scripts/cnode-helper-scripts/guild-deploy.sh
+++ scripts/cnode-helper-scripts/guild-deploy.sh
@@ -397,7 +397,7 @@ download_cncli() {
   [[ -z ${ARCH##*aarch64*} ]] && err_exit "  The cncli pre-compiled binary is not available for ARM, you might need to build them!"
   echo -e "\nInstalling CNCLI.."
   if command -v cncli >/dev/null; then cncli_version="v$(cncli -V 2>/dev/null | cut -d' ' -f2)"; else cncli_version="v0.0.0"; fi
-  cncli_git_version="$(curl -s https://api.github.com/repos/${G_ACCOUNT}/cncli/releases/latest | jq -r '.tag_name')"
+  cncli_git_version="$(curl -s https://api.github.com/repos/cardano-community/cncli/releases/latest | jq -r '.tag_name')"
   echo -e "\n  Downloading CNCLI..."
   rm -rf /tmp/cncli-bin && mkdir /tmp/cncli-bin
   pushd /tmp/cncli-bin >/dev/null || err_exit
