71d52d7412af47de54af80de7aafe24439d2547c
Ola [AHLNET] <ola.ahlman@gmail.com>
2025-03-10 16:15:31 +0100

    CNTools 13.4.0
    ## Description
    - Governance proposal list was updated to be more performant by only
    loading and processing data as you navigate.
    - Support was added for the pool Calidus key according to CIP-88
    standard.
    - fix for kesExpiration helper function
    
    ---------
    
    Co-authored-by: KoT_B_KocMoce <49576827+hodlonaut@users.noreply.github.com>

--- scripts/cnode-helper-scripts/cntools.sh
+++ scripts/cnode-helper-scripts/cntools.sh
@@ -1862,9 +1862,10 @@ function main {
 						" ) Rotate   - rotate pool KES keys"\
 						" ) Decrypt  - remove write protection and decrypt pool"\
 						" ) Encrypt  - encrypt pool cold keys and make all files immutable"\
+						" ) Calidus  - register / rotate pool calidus keys (CIP-88 v2 hot pool keys)"\
 						"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
           println DEBUG " Select Pool Operation\n"
-          select_opt "[n] New" "[i] Import" "[r] Register" "[m] Modify" "[x] Retire" "[l] List" "[s] Show" "[o] Rotate" "[d] Decrypt" "[e] Encrypt" "[h] Home"
+          select_opt "[n] New" "[i] Import" "[r] Register" "[m] Modify" "[x] Retire" "[l] List" "[s] Show" "[o] Rotate" "[d] Decrypt" "[e] Encrypt" "[c] Calidus" "[h] Home"
           case $? in
             0) SUBCOMMAND="new" ;;
             1) SUBCOMMAND="import" ;;
@@ -1876,7 +1877,8 @@ function main {
             7) SUBCOMMAND="rotate" ;;
             8) SUBCOMMAND="decrypt" ;;
             9) SUBCOMMAND="encrypt" ;;
-            10) break ;;
+            10) SUBCOMMAND="calidus" ;;
+            11) break ;;
           esac
           case $SUBCOMMAND in
             new)
@@ -2666,7 +2668,7 @@ function main {
               echo
               println DEBUG "Select pool to retire"
               if [[ ${op_mode} = "online" ]]; then
-                selectPool "${pool_filter}" "${POOL_COLDKEY_VK_FILENAME}"
+                selectPool "all" "${POOL_COLDKEY_VK_FILENAME}"
                 case $? in
                   1) waitToProceed; continue ;;
                   2) continue ;;
@@ -2677,7 +2679,7 @@ function main {
                   3) println ERROR "${FG_RED}ERROR${NC}: signing keys missing from pool!" && waitToProceed && continue ;;
                 esac
               else
-                selectPool "${pool_filter}" "${POOL_COLDKEY_VK_FILENAME}"
+                selectPool "all" "${POOL_COLDKEY_VK_FILENAME}"
                 case $? in
                   1) waitToProceed; continue ;;
                   2) continue ;;
@@ -3311,6 +3313,183 @@ function main {
                 println DEBUG "${FG_BLUE}INFO${NC}: pool files are now protected"
                 println DEBUG "Use 'POOL >> DECRYPT / UNLOCK' to unlock"
               fi
+              waitToProceed && continue
+              ;; ###################################################################
+            calidus)
+              clear
+              println DEBUG "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+              println " >> POOL >> CALIDUS"
+              println DEBUG "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+              [[ ! $(ls -A "${POOL_FOLDER}" 2>/dev/null) ]] && echo && println "${FG_YELLOW}No pools available!${NC}" && waitToProceed && continue
+              [[ ! $(ls -A "${WALLET_FOLDER}" 2>/dev/null) ]] && echo && println "${FG_YELLOW}No wallets available to pay for pool calidus metadata registration!${NC}" && waitToProceed && continue
+              if ! cmdAvailable "cardano-signer" &>/dev/null; then
+                println ERROR "\n${FG_RED}ERROR${NC}: prerequisite tool cardano-signer missing or not executable, please install using ${FG_LGRAY}guild-deploy.sh${NC}"
+                waitToProceed && continue
+              fi
+              cardano_signer_version=$(cardano-signer -version)
+              if ! versionCheck "1.24.0" "${cardano_signer_version##* }"; then
+                println INFO "${FG_YELLOW}Please upgrade cardano-signer, this feature require at least version 1.23.0, found ${cardano_signer_version##* }!${NC}"; waitToProceed && continue
+              fi
+              if [[ ${CNTOOLS_MODE} = "OFFLINE" ]]; then
+                println ERROR "${FG_RED}ERROR${NC}: CNTools started in offline mode, option not available!"
+                waitToProceed && continue
+              else
+                if ! selectOpMode; then continue; fi
+              fi
+              echo
+              println DEBUG "Select pool to create / rotate calidus keys for"
+              if [[ ${op_mode} = "online" ]]; then
+                selectPool "all" "${POOL_COLDKEY_VK_FILENAME}"
+                case $? in
+                  1) waitToProceed; continue ;;
+                  2) continue ;;
+                esac
+                getPoolType ${pool_name}
+                case $? in
+                  2) println ERROR "${FG_RED}ERROR${NC}: signing keys encrypted, please decrypt before use!" && waitToProceed && continue ;;
+                  3) println ERROR "${FG_RED}ERROR${NC}: signing keys missing from pool!" && waitToProceed && continue ;;
+                esac
+              else
+                selectPool "all" "${POOL_COLDKEY_VK_FILENAME}"
+                case $? in
+                  1) waitToProceed; continue ;;
+                  2) continue ;;
+                esac
+                getPoolType ${pool_name}
+              fi
+              metatype="no-schema"
+              calidus_sk_file="${POOL_FOLDER}/${pool_name}/${POOL_CALIDUS_SK_FILENAME}"
+              calidus_vk_file="${POOL_FOLDER}/${pool_name}/${POOL_CALIDUS_VK_FILENAME}"
+              calidus_reg_file="${POOL_FOLDER}/${pool_name}/${POOL_CALIDUS_REG_FILENAME}"
+              if [[ -f ${calidus_reg_file} ]]; then
+                CS_CIP88_META_VERIFY=(
+                  cardano-signer verify
+                  --cip88
+                  --data-file "${calidus_reg_file}"
+                )
+                println ACTION "${CS_CIP88_META_VERIFY[*]}"
+                if ! stdout=$("${CS_CIP88_META_VERIFY[@]}" 2>&1) || [[ ${stdout} = *false* ]]; then
+                  println ERROR "\n${FG_RED}ERROR${NC}: failure during calidus metadata verification!\n  result: ${stdout}\n"
+                  select_opt "[d] Delete metadata file and continue" "[Esc] Return"
+                  case $? in
+                    0) safeDel "${calidus_reg_file}" ;;
+                    1) continue ;;
+                  esac
+                else
+                  println DEBUG "\nCalidus metadata registration found and is valid, continue to submit?"
+                  select_opt "[y] Yes" "[n] No, delete and create a new" "[Esc] Return"
+                  case $? in
+                    0) : ;;
+                    1) safeDel "${calidus_reg_file}" ;;
+                    2) continue ;;
+                  esac
+                fi
+              fi
+              if [[ ! -f ${calidus_reg_file} ]]; then
+                if [[ -f ${calidus_vk_file} ]]; then
+                  println DEBUG "\nCalidus keys already exist, how do you want to proceed?"
+                  select_opt "[k] Keep existing keys" "[o] Overwrite to rotate keys" "[Esc] Return"
+                  case $? in
+                    0) : ;;
+                    1) safeDel "${calidus_sk_file}"; safeDel "${calidus_vk_file}" ;;
+                    2) continue ;;
+                  esac
+                fi
+                CS_CALIDUS_KEYS=(
+                  cardano-signer keygen
+                  --path calidus
+                  --out-skey "${calidus_sk_file}"
+                  --out-vkey "${calidus_vk_file}"
+                )
+                println ACTION "${CS_CALIDUS_KEYS[*]}"
+                if ! stdout=$("${CS_CALIDUS_KEYS[@]}" 2>&1); then
+                  println ERROR "\n${FG_RED}ERROR${NC}: failure during calidus key creation!\n${stdout}"
+                  waitToProceed && continue
+                fi
+                current_slot=$(getSlotTipRef)
+                CS_CIP88_META_FILE=(
+                  cardano-signer sign
+                  --cip88
+                  --calidus-public-key "${calidus_vk_file}"
+                  --secret-key "${pool_coldkey_sk_file}"
+                  --nonce ${current_slot}
+                  --json
+                  --out-file "${calidus_reg_file}"
+                )
+                if [[ ${op_mode} = "hybrid" && ! -f "${calidus_reg_file}" ]]; then
+                  println INFO "\n1. Move '${FG_LGRAY}${calidus_vk_file}${NC}' to offline machine that contain ${FG_GREEN}${pool_name}${NC} pool cold key."\
+                    "2. Run below command to generate registration metadata replacing paths as needed."\
+                    "3. Move generated '${FG_LGRAY}${POOL_CALIDUS_REG_FILENAME}${NC}' file back into ${FG_GREEN}${pool_name}${NC} pool folder on this machine."\
+                    "4. Rerun this command to complete calidus key registration keeping existing keys.\n"
+                  println INFO "${FG_LGRAY}${CS_CIP88_META_FILE[*]}${NC}"
+                  waitToProceed && continue
+                fi
+                println ACTION "${CS_CIP88_META_FILE[*]}"
+                if ! stdout=$("${CS_CIP88_META_FILE[@]}" 2>&1); then
+                  println ERROR "\n${FG_RED}ERROR${NC}: failure during calidus metadata generation!\n${stdout}"
+                  waitToProceed && continue
+                fi
+              fi
+              println DEBUG "\nSelect wallet for calidus registration transaction fee"
+              if [[ ${op_mode} = "online" ]]; then
+                selectWallet "balance"
+                case $? in
+                  1) waitToProceed; continue ;;
+                  2) continue ;;
+                esac
+                getWalletType ${wallet_name}
+                case $? in
+                  2) println ERROR "${FG_RED}ERROR${NC}: signing keys encrypted, please decrypt before use!" && waitToProceed && continue ;;
+                  3) println ERROR "${FG_RED}ERROR${NC}: payment and/or stake signing keys missing from wallet!" && waitToProceed && continue ;;
+                esac
+              else
+                selectWallet "balance"
+                case $? in
+                  1) waitToProceed; continue ;;
+                  2) continue ;;
+                esac
+                getWalletType ${wallet_name}
+              fi
+              getWalletBalance ${wallet_name} true true true true
+              if [[ ${pay_lovelace} -gt 0 && ${base_lovelace} -gt 0 ]]; then
+                # Both payment and base address available with funds, let user choose what to use
+                println DEBUG "\nSelect wallet address to use"
+                if [[ -n ${wallet_count} && ${wallet_count} -gt ${WALLET_SELECTION_FILTER_LIMIT} ]]; then
+                  println DEBUG "$(printf "%s\t\t${FG_LBLUE}%s${NC} ADA" "Base Funds :"  "$(formatLovelace ${base_lovelace})")"
+                  println DEBUG "$(printf "%s\t${FG_LBLUE}%s${NC} ADA" "Payment Funds :"  "$(formatLovelace ${pay_lovelace})")"
+                fi
+                select_opt "[b] Base (default)" "[e] Payment" "[Esc] Cancel"
+                case $? in
+                  0) addr="${base_addr}"; lovelace=${base_lovelace} ;;
+                  1) addr="${pay_addr}";  lovelace=${pay_lovelace} ;;
+                  2) continue ;;
+                esac
+              elif [[ ${pay_lovelace} -gt 0 ]]; then
+                addr="${pay_addr}"
+                lovelace=${pay_lovelace}
+                if [[ -n ${wallet_count} && ${wallet_count} -gt ${WALLET_SELECTION_FILTER_LIMIT} ]]; then
+                  println DEBUG "\n$(printf "%s\t${FG_LBLUE}%s${NC} ADA" "Payment Funds :"  "$(formatLovelace ${pay_lovelace})")"
+                fi
+              elif [[ ${base_lovelace} -gt 0 ]]; then
+                addr="${base_addr}"
+                lovelace=${base_lovelace}
+                if [[ -n ${wallet_count} && ${wallet_count} -gt ${WALLET_SELECTION_FILTER_LIMIT} ]]; then
+                  println DEBUG "\n$(printf "%s\t\t${FG_LBLUE}%s${NC} ADA" "Base Funds :"  "$(formatLovelace ${base_lovelace})")"
+                fi
+              else
+                println ERROR "\n${FG_RED}ERROR${NC}: no funds available for wallet ${FG_GREEN}${wallet_name}${NC}"
+                waitToProceed && continue
+              fi
+              metafile="${calidus_reg_file}"
+              if ! sendMetadata; then
+                safeDel "${calidus_sk_file}"; safeDel "${calidus_vk_file}"
+                waitToProceed && continue
+              fi
+              rm -f "${calidus_reg_file}"
+              echo
+              if ! verifyTx ${addr}; then waitToProceed && continue; fi
+              echo
+              println "Pool calidus key registration metadata successfully posted on-chain"
               waitToProceed && continue
               ;; ###################################################################
           esac # pool sub OPERATION
@@ -4133,25 +4312,36 @@ function main {
                     println " >> VOTE >> GOVERNANCE >> LIST PROPOSALS"
                     println DEBUG "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                     echo
-                    tput sc && println DEBUG "Querying for list of proposals...\n"
-                    getAllGovActions
+                    tput sc && println DEBUG "Querying for number of active proposals...\n"
+                    if ! getActiveGovActionCount; then
+                      println "${FG_RED}Failed to grab list of proposals!${NC}"
+                      waitToProceed && continue
+                    fi
                     tput rc && tput ed
-                    action_cnt=${#vote_action_list[@]}
-                    if [[ ${action_cnt} -eq 0 ]]; then
+                    if [[ ${vote_action_count} -eq 0 ]]; then
                       println "${FG_YELLOW}No active proposals to vote on!${NC}"
                       waitToProceed && continue
                     fi
-                    if [[ ${action_cnt} -gt 3 ]]; then
-                      getAnswerAnyCust page_entries "${action_cnt} proposals found. Enter number of actions to display per page (enter for 3)"
-                    fi
-                    page_entries=${page_entries:=3}
-                    if ! isNumber ${page_entries} || [[ ${page_entries} -lt 1 ]]; then
-                      println ERROR "${FG_RED}ERROR${NC}: invalid number"
-                      waitToProceed && continue
-                    fi
+                    println "${FG_LBLUE}${vote_action_count}${NC} active proposals to vote on found!"
+                    waitToProceed
+                    page_entries=2
                     curr_epoch=$(getEpoch)
                     page=1
-                    pages=$(( (action_cnt + (page_entries - 1)) / page_entries ))
+                    pages=$(( (vote_action_count + (page_entries - 1)) / page_entries ))
+                    start_idx=$(( (page *  page_entries) - page_entries ))
+                    end_idx=$(( page_entries * page )); [[ ${end_idx} -gt ${vote_action_count} ]] && end_idx=${vote_action_count}
+                    tput sc && println DEBUG "Querying for proposals [$((start_idx+1))-${end_idx}]...\n"
+                    getAllGovActions ${page_entries} ${start_idx} true
+                    tput rc && tput ed
+                    action_cnt=${#vote_action_list[@]}
+                    if [[ ${action_cnt} -eq 0 ]]; then
+                      println "${FG_RED}Failed to grab proposals!${NC}"
+                      waitToProceed && continue
+                    fi
+                    vote_action_list_ui=()
+                    for vote_action in "${vote_action_list[@]}"; do
+                      vote_action_list_ui+=( "${vote_action}" )
+                    done
                     while true; do
                       clear
                       if [[ ${show_details} = Y ]]; then
@@ -4186,9 +4376,9 @@ function main {
                       total_len=$(( max_len + 13 + 5 ))
                       border_line="|$(printf "%${total_len}s" "" | tr " " "=")|" # max value length + longest title (13) + spacing (5)
                       println DEBUG "Current epoch : ${FG_LBLUE}$(getEpoch)${NC}"
-                      println DEBUG "Proposals     : ${FG_LBLUE}${action_cnt}${NC}"
+                      println DEBUG "Proposals     : ${FG_LBLUE}${vote_action_count}${NC}"
                       idx=1
-                      for vote_action in "${vote_action_list[@]:${start_idx}:${page_entries}}"; do
+                      for vote_action in "${vote_action_list_ui[@]:${start_idx}:${page_entries}}"; do
                         println DEBUG "\n${border_line}"
                         # calculate length of strings
                         IFS=',' read -r action_id action_type proposed_in expires_after anchor_url drep_yes drep_yes_power drep_yes_pct drep_no drep_no_power drep_no_pct spo_yes spo_yes_power spo_yes_pct spo_no spo_no_power spo_no_pct cc_yes cc_yes_pct cc_no cc_no_pct drep_vt spo_vt cc_vt isParameterSecurityGroup <<< "${vote_action}"
@@ -4389,8 +4579,25 @@ function main {
                             [[ ${action_id} = gov_action* ]] && parseGovActionId ${action_id} || IFS='#' read -r action_tx_id action_idx <<< "${action_id}"
                             ! isNumber "${action_idx}" && println ERROR "\n${FG_RED}ERROR${NC}: invalid action id!" && waitToProceed && continue
                             show_details=Y
+                            continue
                             ;;
                       esac
+                      if [[ -n ${hasNext} && ${key} == n ]]; then
+                        clear
+                        start_idx=$(( (page *  page_entries) - page_entries ))
+                        end_idx=$(( page_entries * page )); [[ ${end_idx} -gt ${vote_action_count} ]] && end_idx=${vote_action_count}
+                        [[ ${#vote_action_list_ui[@]} -ge ${end_idx} ]] && continue
+                        println DEBUG "Querying for proposals [$((start_idx+1))-${end_idx}]...\n"
+                        getAllGovActions ${page_entries} ${start_idx} false
+                        action_cnt=${#vote_action_list[@]}
+                        if [[ ${action_cnt} -eq 0 ]]; then
+                          println "${FG_RED}Failed to grab proposals!${NC}"
+                          waitToProceed && continue 2
+                        fi
+                        for vote_action in "${vote_action_list[@]}"; do
+                          vote_action_list_ui+=( "${vote_action}" )
+                        done
+                      fi
                     done
                     ;; ###################################################################
                   vote)
