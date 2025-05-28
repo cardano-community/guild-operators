056ad56e427286e66f291d05d134d7e6dd31a8ab
Adam Matthews <52178922+adamsthws@users.noreply.github.com>
2025-05-28 21:45:00 +0100

    Sidecar healthchecks updates (#6)
    * Koios api bugfix and error handling
    
    Error handling...
    
    When you hit Koios api errors such as rate limits the script would return this error:
    ```parse error: Invalid numeric literal at line 1, column 9"```
    
    The reason for this was the Koios api query returns the error message as a string, rather than valid JSON so JQ encounters a parsing error.
    
    This update handles the above issue and improves logging output for the operator to understand what went wrong.
    
    ......
    
    Bugfix...
    
    Bugfix: parsing koios api headers
    
    The Koios api headers were not being parsed correctly meaning requests were unauthenticated without the api key and so hitting the lower api rate limits.
    
    * Add healthcheck for logMonitor.sh
    
    Add logmonitor health check function to verify logmonitor.sh has the ability to process node logs and write to the blocklog db.
    
    The new check_logmonitor function verifies that the logMonitor.sh process is working by:
    - Confirming the logMonitor.sh process is active
    - Validating the node's JSON log file exists and is accessible
    - When BLOCKLOG_ENABLED=true, checking that the blocklog database exists and has proper write permissions
    
    * Add healthcheck for ptsendslots
    
    Add ptsendslots health check function
    
    The new check_cncli_ptsendslots() function verifies that "cncli.sh ptsendslots" is working by:
    - Confirming the process is active
    - Validating the required vars are available.
    - Checking the cncli db is available and readable.
    
    * Update comments, log line, typos etc
    
    - Added or updated comments describing functions.
    - Fixed a few small typos in comments.
    - Punctuated log line to improve readability.
    (The "checking health for process" log line is followed by more output so the full stop was required to create separation between sentences)
    
    Update healthcheck.sh
    
    * Refactor check_cncli_sendslots: Extract check_cncli_status functionality
    
    Refactor the check_cncli_sendslots function: extracting the check_cncli_status functionality into its own separate function to improve code readability, modularity and making it easier to maintain.
    
    * Minor refactor of check_cncli_status()
    
    - Code refactored to remove repeated code (DRY principal).
    
    - Minor update to comment wording.
    
    * Bugfix - undefined variable
    
    -Fixes an issue where an undefined variable was mistakenly used in the error log line.
    
    -Variable scope added/amended.
    
    * Fix for KOIOS_API_HEADERS quoting issue
    
    Fixes a quoting issue in the KOIOS_API_HEADERS array. (redundantly quoting all elements as a single string).
    
    This led to the the koios api call being unauthenticated and so hitting the rate limit of 5k request per day which is the 'public' (unauthenticated) tier, even though a valid KOIOS_API_TOKEN was defined.
    
    * Updated Koios api call to make use of KOIOS_API_HEADERS from env
    
    Updated the Koios api call to make use of the KOIOS_API_HEADERS array as set in env (in alignment with the koios api calls in other parts of the project).

--- files/docker/node/addons/healthcheck.sh
+++ files/docker/node/addons/healthcheck.sh
@@ -7,13 +7,13 @@
 ######################################
 
 ENTRYPOINT_PROCESS="${ENTRYPOINT_PROCESS:-cnode.sh}"            # Get the script from ENTRYPOINT_PROCESS or default to "cnode.sh" if not set
-HEALTHCHECK_CPU_THRESHOLD="${HEALTHCHECK_CPU_THRESHOLD:-80}"    # The CPU threshold to warn about if the sidecar process exceeds this for more than 60 seconds, defaults to 80%.
+HEALTHCHECK_CPU_THRESHOLD="${HEALTHCHECK_CPU_THRESHOLD:-80}"    # The CPU threshold to warn about if the sidecar process exceeds this for more than 60 seconds, defaults to 80%
 HEALTHCHECK_RETRIES="${HEALTHCHECK_RETRIES:-20}"                # The number of retries if tip is not incrementing, or cpu usage is over the threshold
 HEALTHCHECK_RETRY_WAIT="${HEALTHCHECK_RETRY_WAIT:-3}"           # The time (in seconds) to wait between retries
 NODE_ALLOWED_DRIFT="${NODE_ALLOWED_DRIFT:-6}"                   # The allowed drift in blocks for the node to be considered in sync
 DB_SYNC_ALLOWED_DRIFT="${DB_SYNC_ALLOWED_DRIFT:-3600}"          # The allowed drift in seconds for the DB to be considered in sync
 CNCLI_DB_ALLOWED_DRIFT="${CNCLI_DB_ALLOWED_DRIFT:-300}"         # The allowed drift in slots for the CNCLI DB to be considered in sync
-CNCLI_SENDTIP_LOG_TIMEOUT="${CNCLI_SENDTIP_LOG_TIMEOUT:-119}"   # log capturing timeout (should one second be lower than container healthcheck '--timeout', which defaults to 120)
+CNCLI_SENDTIP_LOG_TIMEOUT="${CNCLI_SENDTIP_LOG_TIMEOUT:-119}"   # log capture timeout (should be one second lower than container healthcheck '--timeout', which defaults to 120)
 CNCLI_SENDTIP_ALLOWED_DRIFT="${CNCLI_SENDTIP_ALLOWED_DRIFT:-3}" # The allowable difference of the tip moving before it's sent to Pooltool. (Not every tip progression is sent to Pooltool)
 
 ######################################
@@ -25,44 +25,36 @@ CNCLI_SENDTIP_ALLOWED_DRIFT="${CNCLI_SENDTIP_ALLOWED_DRIFT:-3}" # The allowable 
 [[ ! -f "${PARENT}"/env ]] && echo -e "\nCommon env file missing in \"${PARENT}\", please ensure latest guild-deploy.sh was run and this script is being run from ${CNODE_HOME}/scripts folder! \n" && exit 1
 . "${PARENT}"/env offline
 
-if [[ -z "${KOIOS_API_HEADERS[*]}" ]] ; then
-    if [[ -n "${KOIOS_API_TOKEN}" ]] ; then
-        KOIOS_API_HEADERS=(-H "'Authorization: Bearer ${KOIOS_API_TOKEN}'")
-    else
-        KOIOS_API_HEADERS=()
-    fi
-fi
-
-
 # Define a mapping of scripts to their corresponding health check functions
 declare -A PROCESS_TO_HEALTHCHECK
 PROCESS_TO_HEALTHCHECK=(
     ["dbsync.sh"]="check_db_sync"
     ["cnode.sh"]="check_node"
     ["cncli.sh"]="check_cncli"
+    ["logMonitor.sh"]="check_logmonitor"
 )
 
 # FUNCTIONS
+
+# Function to start the relevant health check based on the subcommand of cncli.sh
 check_cncli() {
     cncli_pid=$(pgrep -f "${ENTRYPOINT_PROCESS}")
     cncli_subcmd=$(ps -p "${cncli_pid}" -o cmd= | awk '{print $NF}')
 
-    if [[ "${cncli_subcmd}" != "ptsendtip" ]]; then
-        if check_cncli_db ; then
-            return 0
-        else
-            return 1
-        fi
-    else
-        if check_cncli_sendtip; then
-            return 0
-        else
-            return 1
-        fi
-    fi
+    case "${cncli_subcmd}" in
+    ptsendtip)
+        check_cncli_sendtip
+        ;;
+    ptsendslots)
+        check_cncli_sendslots
+        ;;
+    *)
+        check_cncli_db
+        ;;
+    esac
 }
 
-
+# Function to check if the cncli sqlite db is in sync with the node
 check_cncli_db() {
     CCLI=$(which cardano-cli)
     SQLITE=$(which sqlite3)
@@ -77,7 +69,6 @@ check_cncli_db() {
         return 1
     fi
 }
-
 
 # Function to check if the tip is successfully being sent to Pooltool
 check_cncli_sendtip() {
@@ -125,9 +116,87 @@ check_cncli_sendtip() {
     fi
 }
 
+# Function to check if the cncli status is ok. (Stdout is "ok" if status="ok", error message if not)
+check_cncli_status() {
+    CNCLI=$(which cncli)
+    SQLITE=$(which sqlite3)
+    # Get the required genesis file paths from the config file
+    local -A genesis_files
+    for genesis_file in "ByronGenesisFile" "ShelleyGenesisFile"; do
+        if [[ "${CONFIG##*.}" = "yaml" ]]; then
+            [[ $(grep "${genesis_file}.*\.json" "${CONFIG}") =~ ${genesis_file}:.\"(.+\.json)\" ]] && genesis_files["${genesis_file}"]="${BASH_REMATCH[1]}"
+        elif [[ "${CONFIG##*.}" = "json" ]]; then
+            genesis_files["${genesis_file}"]=$(jq -r ".${genesis_file}" "${CONFIG}")
+        fi
+    done
+    # Check if the genesis path has been retrieved from the config file and that the files exist
+    for genesis_type in "Byron" "Shelley"; do
+        if [[ -n "${genesis_files[${genesis_type}GenesisFile]}" ]] && [[ ! -f "${genesis_files[${genesis_type}GenesisFile]}" ]]; then
+            genesis_error_message="\"${genesis_type}\" genesis file is either not defined in the config file or the genesis file does not exist"
+            echo " \"${genesis_error_message}\": ${genesis_files[${genesis_type}GenesisFile]}"
+            return 1
+        fi
+    done
+    # Check that CNCLI_DB file is available and readable
+    if [[ -z "${CNCLI_DB}" ]]; then
+        CNCLI_DB="${CNODE_HOME}/guild-db/cncli/cncli.db"
+    fi
+    if [[ ! -f "${CNCLI_DB}" || ! -r "${CNCLI_DB}" ]]; then
+        echo "Error: CNCLI_DB file does not exist or is not readable: ${CNCLI_DB}"
+        return 1
+    fi
+    # Verify database can be queried with SQLite
+    if ! timeout 10 "${SQLITE}" "${CNCLI_DB}" ".exit"; then
+        echo "Error: Cannot query CNCLI_DB: ${CNCLI_DB}"
+        return 1
+    fi
+    # Check cncli status
+    local cncli_status
+    cncli_status=$(${CNCLI} status \
+        --byron-genesis "${genesis_files[ByronGenesisFile]}" \
+        --shelley-genesis "${genesis_files[ShelleyGenesisFile]}" \
+        --db "${CNCLI_DB}" |
+        jq -r .status)
+    if [[ $? -eq 0 ]]; then
+        echo "$cncli_status"
+        return 0
+    else
+        echo "Error: Status check failed when running \"cncli status\" command"
+        return 1
+    fi
+}
 
+# Function to check if the cncli sendslots process is running
+check_cncli_sendslots() {
+    # Check that the "cncli.sh ptsendslots" process is running
+    if ! pgrep -f "cncli.sh ptsendslots" >/dev/null; then
+        echo "Error: sendslots process is not running"
+        return 1
+    fi
+    # Check $PT_API_KEY is available and valid
+    if [[ -z "${PT_API_KEY}" ]] || [[ ! "${PT_API_KEY}" =~ ^[a-z0-9-]+$ ]]; then
+        echo "Error: PT_API_KEY is not set or not valid (must be lowercase alphanumeric and hyphen chars only)"
+        return 1
+    fi
+    # Check $POOL_TICKER is available and valid
+    if [[ -z "${POOL_TICKER}" ]] || [[ ! "${POOL_TICKER}" =~ ^[A-Z0-9]{3,5}$ ]]; then
+        echo "Error: POOL_TICKER is not set or not valid (must be 3-5 characters in length, A-Z and 0-9 only)"
+        return 1
+    fi
+    # Check cncli status
+    local cncli_status
+    cncli_status="$(check_cncli_status)"
+    if [[ "${cncli_status}" != "ok" ]]; then
+        echo "Error when checking cncli status: \"${cncli_status}\""
+        return 1
+    else
+        echo "Ready to send slots to PoolTool at the beginning of next epoch"
+        return 0
+    fi
+}
+
+# Check if the Postgres DB is in sync
 check_db_sync() {
-    # Check if the DB is in sync
     [[ -z "${PGPASSFILE}" ]] && PGPASSFILE="${CNODE_HOME}/priv/.pgpass"
     if [[ ! -f "${PGPASSFILE}" ]]; then
         echo "ERROR: The PGPASSFILE (${PGPASSFILE}) not found, please ensure you've followed the instructions on guild-operators website!" && exit 1
@@ -148,7 +217,6 @@ check_db_sync() {
         return 1
     fi
 }
-
 
 # Function to check if the node is running and is on tip
 check_node() {
@@ -174,7 +242,18 @@ check_node() {
             return 1
         fi
     else
-        SECOND=$($CURL -s "${KOIOS_API_HEADERS[@]}" "${URL}" | $JQ '.[0].block_no')
+    # Query the Koios API and check if the response is valid
+    KOIOS_RESPONSE=$(${CURL} -s "${KOIOS_API_HEADERS[@]}" "${URL}")
+    if ! echo "${KOIOS_RESPONSE}" | $JQ . &>/dev/null; then
+        echo "Error. Koios API query output: \"${KOIOS_RESPONSE}\""
+        return 1
+    else
+        SECOND=$(echo "${KOIOS_RESPONSE}" | $JQ '.[0].block_no')
+        if ! [[ "$SECOND" =~ ^[0-9]+$ ]]; then
+            echo "Error. Failed to get valid block height from Koios API"
+            return 1
+        fi
+    fi
 
         for (( CHECK=0; CHECK<=HEALTHCHECK_RETRIES; CHECK++ )); do
             # Set BIDIRECTIONAL_DRIFT to 1 since using an API call
@@ -217,7 +296,6 @@ check_process() {
     return 1  # Return 1 if retries are exhausted
 }
 
-
 # Function to check if the tip is progressing or within the allowed drift.
 # FIRST_TIP: The first tip to compare
 # SECOND_TIP: The second tip to compare
@@ -226,7 +304,6 @@ check_process() {
 #                      tips. This is useful when the tip can move backwards, like with API calls.
 # Returns 0 if the diff of the second tip minus the first tip is between 0 and the allowed drift.
 # Returns 1 if the diff is outside the allowed drift.
-
 check_tip() {
     FIRST_TIP=$1
     SECOND_TIP=$2
@@ -254,6 +331,50 @@ check_tip() {
     fi
 }
 
+# Function to check if logMonitor.sh is running and properly processing logs
+check_logmonitor() {
+    SQLITE=$(which sqlite3)
+    # Check if logMonitor process is running
+    if ! pgrep -f "logMonitor.sh" >/dev/null; then
+        echo "ERROR: logMonitor.sh process is not running"
+        return 1
+    fi
+    # Get the log file that logMonitor is watching from the config file
+    local logfile=""
+    if [[ "${CONFIG##*.}" = "yaml" ]]; then
+        [[ $(grep "scName.*\.json" "${CONFIG}") =~ scName:.\"(.+\.json)\" ]] && logfile="${BASH_REMATCH[1]}"
+    elif [[ "${CONFIG##*.}" = "json" ]]; then
+        logfile=$(jq -r '.setupScribes[] | select (.scFormat == "ScJson") | .scName' "${CONFIG}")
+    fi
+    # Check if the path to the log file is present
+    if [[ -z "${logfile}" ]]; then
+        echo "ERROR: Failed to locate json log file."
+        return 1
+    fi
+    # Check if the node log file is present
+    if [[ ! -f "${logfile}" ]]; then
+        echo "ERROR: Node log file does not exist. ${logfile} "
+        return 1
+    fi
+    # Check if the blocklog database is present
+    if [[ ! -f "${BLOCKLOG_DB}" ]]; then
+        echo "ERROR: Block log database ${BLOCKLOG_DB} does not exist"
+        return 1
+    fi
+    # Verify write access to the blocklog database file
+    if [[ ! -w "${BLOCKLOG_DB}" ]]; then
+        echo "ERROR: No write permission to block log database ${BLOCKLOG_DB}"
+        return 1
+    fi
+    # Verify the database is readable with SQLite
+    if ! timeout 10 "${SQLITE}" "${BLOCKLOG_DB}" ".exit"; then
+        echo "ERROR: Cannot connect to block log database ${BLOCKLOG_DB}"
+        return 1
+    fi
+    # If all checks pass
+    echo "Healthy: logMonitor.sh is running and able to process logs"
+    return 0
+}
 
 # MAIN
 if [[ -n "${PROCESS_TO_HEALTHCHECK[$ENTRYPOINT_PROCESS]}" ]]; then
@@ -261,12 +382,11 @@ if [[ -n "${PROCESS_TO_HEALTHCHECK[$ENTRYPOINT_PROCESS]}" ]]; then
     eval "${PROCESS_TO_HEALTHCHECK[$ENTRYPOINT_PROCESS]}"
     exit $?
 else
-    # When 
     # Determine the process name or script to check health
     if [[ -n "${SCRIPT_TO_BINARY_MAP[$ENTRYPOINT_PROCESS]}" ]]; then
         process="${SCRIPT_TO_BINARY_MAP[$ENTRYPOINT_PROCESS]}"
     fi
-    echo "Checking health for process: $process"
+    echo "Checking health for process: $process."
     check_process "$process" "$HEALTHCHECK_CPU_THRESHOLD"
     exit $?
 fi
