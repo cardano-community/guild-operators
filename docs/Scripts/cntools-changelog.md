8799473405025d861a5efbf711f58112f00fd78c
Ola Ahlman <ola.ahlman@gmail.com>
2026-03-18 10:33:02 +0100

    Node/CLI version bump & CNTools updates

--- docs/Scripts/cntools-changelog.md
+++ docs/Scripts/cntools-changelog.md
@@ -6,6 +6,17 @@ All notable changes to this tool will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [13.5.5] - 2026-03-18
+#### Changed
+- Minor changes due to node/cli version bump.
+  - node = 10.6.2
+  - cli  = 10.15.0.1
+- Pool metadata URL now support length of 128 instead of 64.
+
+## [13.5.4] - 2026-02-10 (unreleased)
+#### Fixed
+- Governance fix when in local mode for SPO vote power.
+
 ## [13.5.3] - 2025-12-15
 #### Fixed
 - More governance vote calculation fixes when in local mode.
--- scripts/cnode-helper-scripts/guild-deploy.sh
+++ scripts/cnode-helper-scripts/guild-deploy.sh
@@ -109,8 +109,8 @@ set_defaults() {
   [[ -z "${BRANCH}" ]] && BRANCH="master"
   [[ "${SUDO}" = 'Y' ]] && sudo="sudo" || sudo=""
   [[ "${SUDO}" = 'Y' && $(id -u) -eq 0 ]] && err_exit "Please run as non-root user."
-  [[ -z "${CARDANO_NODE_VERSION}" ]] && CARDANO_NODE_VERSION="$(curl -sfk "https://raw.githubusercontent.com/${G_ACCOUNT}/guild-operators/${BRANCH}/files/docker/node/release-versions/cardano-node-latest.txt" || echo "10.5.3")"
-  [[ -z "${CARDANO_CLI_VERSION}" ]] && CARDANO_CLI_VERSION="$(curl -sfk "https://raw.githubusercontent.com/${G_ACCOUNT}/guild-operators/${BRANCH}/files/docker/node/release-versions/cardano-cli-latest.txt" || echo "10.11.1.0")"
+  [[ -z "${CARDANO_NODE_VERSION}" ]] && CARDANO_NODE_VERSION="$(curl -sfk "https://raw.githubusercontent.com/${G_ACCOUNT}/guild-operators/${BRANCH}/files/docker/node/release-versions/cardano-node-latest.txt" || echo "10.6.2")"
+  [[ -z "${CARDANO_CLI_VERSION}" ]] && CARDANO_CLI_VERSION="$(curl -sfk "https://raw.githubusercontent.com/${G_ACCOUNT}/guild-operators/${BRANCH}/files/docker/node/release-versions/cardano-cli-latest.txt" || echo "10.15.0.1")"
   CNODE_HOME="${CNODE_PATH}/${CNODE_NAME}"
   CNODE_VNAME=$(echo "$CNODE_NAME" | awk '{print toupper($0)}')
   [[ -z ${MITHRIL_HOME} ]] && MITHRIL_HOME="${CNODE_HOME}/mithril"
